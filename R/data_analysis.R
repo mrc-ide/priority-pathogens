91b4996fff22edd0453f9afd0c1fb19c65bdfa86
Kelly McCain <k.mccain22@imperial.ac.uk>
2023-04-05 16:06:59 +0100

    Reorganization of files and editing functions

---
+++ R/data_analysis.R
@@ -0,0 +1,16 @@
+# Data analysis functions
+
+# Figures: 
+# PRISMA flowchart
+# Ruth: Forest-type plot (with vertical line in middle as median) with panels for each parameter class
+  # y axis = study name
+  # x axis = geom_errorbar with a vline that is median  
+  # seroprevalence: colored by assay
+  # epiparameter package ? or another package in epiverse, or meta
+# Bar chart of parameter classes
+# Gina will do dot / line plot for human delays
+# Kelly: Map of seroprevalence overlaid with outbreaks over time (different panels) - 
+  # only Africa
+  # shapes for different assays
+  
+
