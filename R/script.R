af38bb7f56e0850e9f0c92a3a11324f1ab6bba2a
ginacuomo <g.cuomo-dannenburg18@imperial.ac.uk>
2023-04-05 10:35:13 +0100

    fixing issues with column_name to check NAs deleted and updated script

--- R/script.R
+++ R/script.R
@@ -1,13 +1,16 @@
 article_raw <- import('data/marburg/marburg_article.xlsx')
-outbreak_raw <- import('data/marburg/marburg_outbreak.xlsx')
 model_raw <- import('data/marburg/marburg_model.xlsx')
-parameters_raw <- readxl::read_xlsx('data/marburg/marburg_parameter.xlsx')
+outbreak_raw <- import('data/marburg/marburg_outbreak.xlsx')
+parameter_raw <- readxl::read_xlsx('data/marburg/marburg_parameter.xlsx')
 
-
-article_clean <- article_raw %>% clean_dfs(column_name = 'article_name') 
+article_clean <- article_raw %>% clean_dfs(column_name = 'article_title') 
+model_clean <- model_raw %>% clean_dfs(column_name = 'model_type')
+outbreak_clean <- outbreak_raw %>% clean_dfs(column_name = 'outbreak_country')
+parameter_clean <- parameter_raw %>% clean_dfs(column_name = 'parameter_type')
 
 details <- article_clean %>% get_details()
 
-
-
-parameter_clean <- parameters_raw %>% clean_dfs(column_name = 'parameter_type')
+article_clean <- article_clean %>% add_details(detail = details)
+model_clean <- model_clean %>% add_details(detail = details)
+outbreak_clean <- outbreak_clean %>% add_details(detail = details)
+parameter_clean <- parameter_clean %>% add_details(detail = details)
