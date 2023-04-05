b552137b4b73e2a771edf8e4aa45b578efe3454e
ginacuomo <g.cuomo-dannenburg18@imperial.ac.uk>
2023-04-05 10:35:13 +0100

    fixing issues with column_name to check NAs deleted and updated script

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -18,9 +18,9 @@ clean_dfs <- function(df, column_name){
   #' process: clean names, drop rows with NAs, adds a grouping variable for classification of the parameter type
   #' output: df 
   df <- df %>%
-    clean_names() %>%
-    dplyr::filter(!is.na(column_name)) 
-  
+    janitor::clean_names() %>%
+    dplyr::filter(!is.na(.data[[column_name]]))
+
   if('parameter_type' %in% colnames(df)){
     df <- df %>%
       mutate(parameter_class = case_when(grepl('Human delay', parameter_type) ~ 'Human delay',
@@ -43,23 +43,40 @@ get_details <- function(article_df){
   #' output: df with 4 columns
   
   df <- article_df %>%
-    select(c(article_id, covidence_id, name_data_entry)) %>%
-    mutate(double_extracted = ifelse(duplicated(covidence_id) & !duplicated(article_id), 1, 0))
+    dplyr::select(c(article_id, covidence_id, name_data_entry)) %>%
+    dplyr::mutate(double_extracted = ifelse(duplicated(covidence_id) & !duplicated(article_id), 1, 0)) 
   
   return(df)
 }
 
-get_double_extracted <- function(df, detail){
+add_details <- function(df, detail) {
+  df <- df %>%
+    left_join(details, by = 'article_id')
+  return(df)
+}
+
+get_double_extracted <- function(df_with_detail){
   #' input: data frame
   #' process: gets double extracted articles and identifies if matching
   #' output: data frame 
   df <- df %>%
-    left_join(details, by = 'article_id') %>%
-    dplyr::filter(double_extracted == 1) )
-    # mutate use is_matching function 
+    dplyr::filter(double_extracted == 1) %>%
+    dplyr::mutate(double_matching = is_matching())
 }
 
-is_matching <- function(){
+is_matching <- function(df, column_name1, column_name2){
+  #' input: dataframe of double extracted rows, columns that you want to match by
+  #' suggested columns:
+    #' parameter df: parameter type, population location
+    #' article df, NA --> make a separate function for the quality assessment?
+    #' outbreaks df: outbreak_date_year, outbreak_country
+    #' model df: model type, compartmental model (unsure on this one though)
+  #' process: matches the row entries by covidence id and other columns, determines if they differ
+  #' output output 0 if they don't match and 1 if they do
+  df <- df %>%
+    dplyr::group_by(covidence_id, .data[[column_name1]], .data[[column_name2]]) %>%
+    dplyr::summarise(rows = nrow())
+    
   
 }
 
