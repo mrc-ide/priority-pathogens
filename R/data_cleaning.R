008c6b1ae156e4d1a5cdb97c87d6200893c62645
Kelly McCain <k.mccain22@imperial.ac.uk>
2023-04-27 10:40:50 +0100

    updates to map code

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -32,9 +32,9 @@ clean_dfs <- function(df, column_name){
                                          grepl('Severity', parameter_type) ~ 'Severity',
                                          grepl('Mosquito', parameter_type) ~ "Mosquito",
                                          grepl('Relative', parameter_type) ~ 'Relative contribution',
-                                         TRUE ~ 'Other'))%>%
+                                         TRUE ~ 'Other')) %>%
       mutate(population_country = str_replace(population_country, 'Congo, Rep.', 'Republic of the Congo'),
-             population_country = str_replace(population_country, 'Congo, Dem. Rep.', 'Democratic Republic of the Congo'))
+             population_country = str_replace(population_country, 'Congo, Dem. Rep.', 'Democratic Republic of the Congo')) 
   }
   
   return(df)
