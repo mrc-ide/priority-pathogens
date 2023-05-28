42f09ee60c994cc2877edb0a0dea2fe7f40a4dbc
ruthmccabe <ruth.mccabe17@imperial.ac.uk>
2023-05-28 13:52:02 +0200

    short names for parameters in parameter overview plot

--- R/parameter_overview.R
+++ R/parameter_overview.R
@@ -21,7 +21,7 @@ parameter_df$parameter_type <- factor(parameter_df$parameter_type, levels = uniq
 ggplot(parameter_df, aes(x = parameter_type, col = parameter_class, fill = parameter_class)) + 
   geom_bar() + theme_bw() +
   theme(axis.text.x = element_text(angle = 315, vjust = 0, hjust=0)) +
-  labs(x = "Parameter type", y = "Number extracted") + 
+  labs(x = "Parameter", y = "Number extracted") + 
   guides(fill = guide_legend(title="Parameter Classification"), 
          col = guide_legend(title="Parameter Classification")) + 
   scale_y_continuous(breaks = seq(0, 10, by = 2))+
@@ -30,3 +30,60 @@ ggplot(parameter_df, aes(x = parameter_type, col = parameter_class, fill = param
 
 ggsave(filename="data/marburg/output/parameter_overview.png",bg = "white",
        width = 15, height = 10, units = "cm")
+
+
+
+### wanted a version with shorter parameter types
+
+parameter_df <- parameter_df %>%
+  mutate(parameter_type_short = ifelse(parameter_type=="Human delay - time symptom to careseeking",
+                                       "Time symptom to careseeking",
+                                       ifelse(parameter_type=="Human delay - generation time",
+                                              "Generation time",
+                                              ifelse(parameter_type=="Human delay - time symptom to outcome",
+                                                     "Time symptom to outcome",
+                                                     ifelse(parameter_type=="Human delay - time in care",
+                                                            "Time in care",
+                                                            ifelse(parameter_type=="Human delay - incubation period",
+                                                                   "Incubation period",
+                                                                   ifelse(parameter_type=="Mutations - mutation rate",
+                                                                          "Mutation rate",
+                                                                          ifelse(parameter_type=="Mutations - substitution rate","Substitution rate",ifelse(parameter_type=="Mutations - evolutionary rate","Evolutionary rate",      ifelse(parameter_type=="Seroprevalence - IFA","IFA",
+         ifelse(parameter_type=="Seroprevalence - Unspecified","Unspecified",
+                ifelse(parameter_type=="Seroprevalence - IgG","IgG",
+                       ifelse(parameter_type=="Seroprevalence - IgM","IgM",
+                              ifelse(parameter_type=="Seroprevalence - HAI/HI","HAI/HI",
+                                     ifelse(parameter_type=="Severity - case fatality rate (CFR)",
+                                            "Case fatality ratio (CFR)",
+                                            ifelse(parameter_type=="Growth rate (r)","Growth rate (r)",
+                                                   ifelse(parameter_type=="Doubling time","Doubling time",
+                                                          ifelse(parameter_type=="Attack rate","Attack rate",
+                                                                 ifelse(parameter_type=="Risk factors","Risk factors",
+                                                                        ifelse(parameter_type=="Reproduction number (Effective, Re)",
+                                                                               "Effective (Re)",
+                                                                               ifelse(parameter_type=="Reproduction number (Basic R0)","Basic (R0)",
+                                                                                      NA)))))))))))))))))))),
+         parameter_type_short = factor(parameter_type_short,
+                                       levels=c("Time symptom to careseeking","Generation time",
+                                                "Time symptom to outcome","Time in care","Incubation period",
+                                                "Mutation rate","Substitution rate","Evolutionary rate",
+                                                "Growth rate (r)","Doubling time","Attack rate",
+                                                "Effective (Re)","Basic (R0)","Risk factors",
+                                                "IFA","Unspecified","IgG","IgM","HAI/HI",
+                                                "Case fatality ratio (CFR)")))
+
+
+ggplot(parameter_df, aes(x = parameter_type_short, col = parameter_class, fill = parameter_class)) + 
+  geom_bar() + theme_bw() +
+  theme(axis.text.x = element_text(angle = 315, vjust = 0, hjust=0)) +
+  labs(x = "Parameter", y = "Number extracted") + 
+  guides(fill = guide_legend(title="Parameter Classification"), 
+         col = guide_legend(title="Parameter Classification")) + 
+  scale_y_continuous(breaks = seq(0, 10, by = 2))+
+  scale_fill_viridis_d(option = "magma",begin=0.15,end=0.95)+
+  scale_colour_viridis_d(option="magma",begin=0.15,end=0.95)
+
+ggsave(filename="data/marburg/output/parameter_overview_shortnames.png",bg = "white",
+       width = 15, height = 10, units = "cm")
+
+
