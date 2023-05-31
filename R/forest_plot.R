5de9859fa2bd363bb63fa2a26e21e21d81093b7b
cm401 <christian.morgenstern@gmail.com>
2023-05-31 11:09:58 +0100

    make generic and move marburg code out

--- R/forest_plot.R
+++ R/forest_plot.R
@@ -1,27 +1,8 @@
 ## function to produce forest plot
-## to go into data_analysis.R when up and running
-
-# doing on 'parameter_single' until get finalised data
-
-REGENERATE_DATA <- FALSE
-
-if(REGENERATE_DATA) source("R/script.R")              #only rerun this if we want to re-generate the marburg files (rather than rewriting this every time) 
-
 library(ggplot2)
 library(tidyverse)
 library(patchwork)
 library(cowplot)
-
-df <- read.csv("data/marburg/final/parameter_final.csv")
-article_df <- read.csv("data/marburg/final/article_final.csv")
-
-# merge with article ID to get the y-axis labels
-df <- merge(df,article_df %>% dplyr::select(article_id,first_author_first_name,year_publication),
-            all.x=TRUE,by="article_id") %>%
-  mutate(article_label = as.character(paste0(first_author_first_name," ",year_publication))) %>%
-  dplyr::arrange(article_label, -year_publication) %>%
-  dplyr::filter(article_id %in% c(17,15) == FALSE) 
-
 
 # fatality rates 
 forest_plot_fr <- function(df) {
@@ -87,7 +68,6 @@ forest_plot_fr <- function(df) {
   return(plot)
   
 }
-
 
 ## reproduction numbers
 forest_plot_R <- function(df){
@@ -279,131 +259,4 @@ forest_plot_mutations <- function(df){
   
   return(plot)
 }
-
-
-human_delay <- forest_plot_delay(df)
-human_delay
-#ggsave(plot = human_delay,filename="data/marburg/output/FP_human_delay.png",bg = "white",width = 25, height = 15, units = "cm")
-
-mutations <- forest_plot_mutations(df)
-mutations
-#ggsave(plot = mutations,filename="data/marburg/output/FP_mutations.png",bg = "white",width = 15, height = 10, units = "cm")
-
-
-reproduction_number <- forest_plot_R(df)
-reproduction_number
-#ggsave(plot = reproduction_number,filename="data/marburg/output/FP_reproduction_number.png",bg = "white",width = 15, height = 10, units = "cm")
-
-
-severity <- forest_plot_fr(df)
-severity
-#ggsave(plot = severity,filename="data/marburg/output/FP_severity.png",bg = "white",width = 15, height = 10, units = "cm")
-
-
-plot_grid(reproduction_number+labs(tag="A"),
-          severity+labs(tag="B"),
-          human_delay+labs(tag="C"),
-          mutations+labs(tag="D"),
-          nrow=2,align="hv",rel_heights = c(0.7,1))
-ggsave(filename="data/marburg/output/panel_plot.png",bg = "white",width = 15, height=10)
-
-
-panel_plot <- (reproduction_number + ggtitle("A") + severity + ggtitle("B")) / (human_delay + ggtitle("C") + mutations + ggtitle("D"))
-ggsave(plot = panel_plot,filename="data/marburg/output/panel_plot.png",bg = "white",width = 15, height=10)
-
-
-# ## the only figure that doesn't yet have a custom figure -- I think this is 
-# # better as a table regardless?
-# seroprevalence <- forest_plot(df,"Seroprevalence")
-# seroprevalence
-# ggsave(plot = seroprevalence,filename="data/marburg/output/FP_seroprevalence.png",bg = "white")
-# 
-
-# for testing purposes
-#parameter <- "Human delay"
-
-# # original code from Ruth
-# forest_plot <- function(df, parameter){
-# 
-#   if(!(parameter %in% c("Human delay","Seroprevalence","Mutations","Risk factors","Reproduction number",
-#                      "Severity","Mosquito","Relative","Other"))){
-#     stop("Parameter class not valid.")
-#   }
-# 
-# 
-#   df_plot <- df %>% filter(parameter_class == parameter) %>%
-#     mutate(parameter_value = as.numeric(parameter_value)) %>%
-#     group_by(parameter_type) %>% ### median function not behaving in ggplot so going with this even with grouping
-#     mutate(median = median(parameter_value,na.rm=TRUE))
-# 
-#   ## need some logic for the different parameter classes
-#   ## can't do for risk factors as not extracting values
-#   ## don't think we can plot seroprevalence this way - needs to include time as well
-#   ## how to distinguish between uncertainty reporting and range reporting
-# 
-#   ## need to see "Relative contribution" to be able to plot
-#   ## 'Mosquito' won't be an issue for Marburg
-#   ## decide what to do with "Other"
-# 
-#   if(parameter %in% c("Human delay","Mutations","Reproduction number","Severity")){
-# 
-#     plot <- ggplot(df_plot)+
-#       theme_bw()+
-#       facet_wrap(~parameter_type,scales="free_x",
-#                  labeller = labeller(parameter_type = label_wrap_gen(width = 25)))+
-#       geom_errorbar(aes(y=article_label,xmin=parameter_lower_bound,xmax=parameter_upper_bound,
-#                         group=parameter_data_id,
-#                         linetype="Parameter range"),
-#                     position=position_dodge(width=0.5),
-#                     width=0.25)+
-#       geom_errorbar(aes(y=article_label,
-#                         xmin=parameter_uncertainty_lower_value,xmax=parameter_uncertainty_upper_value,
-#                         group=parameter_data_id,
-#                         linetype="Uncertainty interval"),
-#                     position=position_dodge(width=0.5),
-#                     width=0.25)+
-#       geom_point(aes(x=parameter_value,y=article_label,group=parameter_data_id),position=position_dodge(width=0.5))+
-#       geom_vline(aes(xintercept=median,col="Sample median"),linetype="dashed")+
-#       labs(x="Parameter value",y="Study (First author surname and publication year)",linetype="",colour="")+
-#       scale_linetype_manual(values = c("dotted","solid"))+
-#       scale_colour_manual(values=c("blue"))+
-#       theme(legend.position="bottom",
-#             strip.text = element_text(size=12))
-# 
-#   }
-# 
-#   if(parameter=="Seroprevalence"){
-# 
-#     plot <- ggplot(df_plot,
-#            aes(x=population_study_end_year,y=parameter_value,group=factor(parameter_data_id)))+
-#       theme_bw()+
-#       facet_wrap(~parameter_type,scales="free_x",
-#                  labeller = labeller(parameter_type = label_wrap_gen(width = 25)))+
-#       geom_errorbar(position=position_dodge2(width=0.5),
-#                     aes(ymin=parameter_lower_bound,ymax=parameter_upper_bound,
-#                         group=parameter_data_id,col=article_label,linetype="Parameter range"))+
-#       geom_errorbar(position=position_dodge2(width=0.5),
-#                     aes(ymin=parameter_uncertainty_lower_value,ymax=parameter_uncertainty_upper_value,
-#                         group=parameter_data_id,col=article_label,linetype="Uncertainty interval"))+
-#       geom_point(position=position_dodge2(width=0.5),
-#                  aes(x=population_study_end_year,y=parameter_value,
-#                      col=article_label,group=parameter_data_id))+
-#       labs(x="Reported end year of study",y="Parameter value",linetype="",colour="Study")+
-#       scale_linetype_manual(values = c("dotted","solid"))+
-#       theme(legend.position="bottom",
-#              legend.box="vertical",
-#             strip.text = element_text(size=12))+
-#       scale_colour_viridis_d(end=0.9)+
-#       guides(colour = guide_legend(order = 2),
-#              linetype = guide_legend(order = 1,byrow=TRUE))
-# 
-#   }
-# 
-#   if(parameter %in% c("Risk factors","Mosquito","Relative","Other")){
-#     stop("Parameter class not yet coded.")
-#   }
-# 
-#   return(plot)
-# 
-# }
 
