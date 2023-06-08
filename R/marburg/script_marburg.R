8923b130b7a0312c88f58c8dd6d4bf92135490c0
cm401 <christian.morgenstern@gmail.com>
2023-06-08 14:57:53 +0100

    add in marburg quality assessment file

--- R/marburg/script_marburg.R
+++ R/marburg/script_marburg.R
@@ -1,9 +1,11 @@
+library(readxl)
 source('R/data_cleaning.R')
 
-article_raw <- import('data/marburg/raw/marburg_article_new.xlsx')
-model_raw <- import('data/marburg/raw/marburg_model_new.xlsx')
-outbreak_raw <- import('data/marburg/raw/marburg_outbreak_new.xlsx')
+article_raw   <- import('data/marburg/raw/marburg_article_new.xlsx')
+model_raw     <- import('data/marburg/raw/marburg_model_new.xlsx')
+outbreak_raw  <- import('data/marburg/raw/marburg_outbreak_new.xlsx')
 parameter_raw <- import('data/marburg/raw/marburg_parameter_new.xlsx')
+quality_raw   <- readxl::read_excel('data/marburg/raw/marburg_quality_assessment.xlsx',sheet="final") %>% mutate(covidence_id=as.double(str_replace(`Covidence #`,"#","")))
 
 real_duplicate <- article_raw %>% filter(duplicated(Covidence_ID)) %>% pull(var = Covidence_ID)
 
@@ -91,7 +93,12 @@ article_final <- rbind(article_single,
                        article_double) %>%
   filter(!is.na(covidence_id))
 
+# we have quality reviews for slightly more papers than final articles (some got excluded afterwards)
+# Covidence_id 1931 dropped as it's a review.
+quality_final <- left_join(quality_raw,article_final %>% filter(!is.na(year_publication)),by=c("covidence_id"))
+
 write.csv(article_final, "data/marburg/final/article_final.csv", row.names = FALSE)
 write.csv(model_final, "data/marburg/final/model_final.csv", row.names = FALSE)
 write.csv(outbreak_final, "data/marburg/final/outbreak_final.csv", row.names = FALSE)
 write.csv(parameter_final, "data/marburg/final/parameter_final.csv", row.names = FALSE) # 24 apr missing Cyril and Gina's DE
+write.csv(quality_final, "data/marburg/final/quality_final.csv")
