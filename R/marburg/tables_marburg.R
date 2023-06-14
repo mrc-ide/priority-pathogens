0903954c470cd53421fdddec89cfa7df9097c72e
cm401 <christian.morgenstern@gmail.com>
2023-06-14 13:41:07 +0100

    need to match against the right version of the articles data

--- R/marburg/tables_marburg.R
+++ R/marburg/tables_marburg.R
@@ -6,8 +6,9 @@ if(REGENERATE_DATA) source("R/marburg/script_marburg.R")              #only reru
 
 source("R/table_code.R")
 
-par_final <- read.csv("data/marburg/final/parameter_final.csv")
-article_df <- read.csv("data/marburg/final/article_final.csv")
+par_final      <- read.csv("data/marburg/final/parameter_final.csv")
+article_df     <- read.csv("data/marburg/final/article_clean.csv")
+article_final  <- read.csv("data/marburg/final/article_final.csv")
 outbreak_final <- read.csv("data/marburg/final/outbreak_final.csv")
 
 # merge with article ID article labels
@@ -19,7 +20,7 @@ df <- merge(par_final, article_df %>% dplyr::select(article_id, first_author_fir
   dplyr::filter(article_id %in% c(17,15) == FALSE) %>%
   rename('Survey year'=Survey.year)
 
-outbreak <- merge(outbreak_final, article_df %>% dplyr::select(covidence_id, first_author_first_name,year_publication),
+outbreak <- merge(outbreak_final, article_final %>% dplyr::select(covidence_id, first_author_first_name,year_publication),
                   all.x=TRUE, by="covidence_id") %>%
   dplyr::filter(is.na(double_extracted) == FALSE) %>%
   mutate(article_label = as.character(paste0(first_author_first_name," ",year_publication)),
