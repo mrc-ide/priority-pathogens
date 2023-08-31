ec20b819865957cdc80dfc310641c1bce6f163df
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2023-08-31 16:05:17 +0100

    Use odbc to connect to Access DB

--- src/db_extraction/orderly.R
+++ src/db_extraction/orderly.R
@@ -10,9 +10,22 @@ orderly_artefact(
     "outbreaks.csv"))
 
 infiles <- orderly_resource(
-  c("DIDE Priority Pathogens EBOLA - SABINE.accdb",
-    "DIDE Priority Pathogens EBOLA - SANGEETA.accdb"
-    )
+  c("DIDE Priority Pathogens EBOLA - ANNE.accdb",
+    "DIDE Priority Pathogens EBOLA - CHRISTIAN.accdb",
+    "DIDE Priority Pathogens EBOLA - CYRIL.accdb",
+    "DIDE Priority Pathogens EBOLA - DARIYA.accdb",
+    "DIDE Priority Pathogens EBOLA - ETTIE.accdb",
+    "DIDE Priority Pathogens EBOLA - GINA.accdb",
+    "DIDE Priority Pathogens EBOLA - JACK.accdb",
+    "DIDE Priority Pathogens EBOLA - JOSEPH.accdb",
+    "DIDE Priority Pathogens EBOLA - KELLY.accdb",
+    "DIDE Priority Pathogens EBOLA - PATRICK.accdb",
+    "DIDE Priority Pathogens EBOLA - REBECCA.accdb",
+    "DIDE Priority Pathogens EBOLA - RUTH.accdb",
+    "DIDE Priority Pathogens EBOLA - SABINE.accdb",
+    "DIDE Priority Pathogens EBOLA - SANGEETA.accdb",
+    "DIDE Priority Pathogens EBOLA - SEQUOIA.accdb"
+  )
 )
 ## pathogen should be set to one of our priority-pathogens
 ## use capital case; see code below where this pathogen
@@ -20,59 +33,28 @@ infiles <- orderly_resource(
 ## Downstream tasks can query on this parameter to
 ## pull in the correct files as dependancies.
 ## Extraction can be single or double.
-## This is so that downsteam tasks can pull in the right
+## This is so that downstream tasks can pull in the right
 ## outputs and put them together.
 orderly_parameters(pathogen = NULL, extraction = "single")
 
 library(dplyr)
-library(here)
-library(Hmisc)
 library(ids)
+library(odbc)
 library(purrr)
 library(readr)
+
 
 ## Extract one access DB at a time
 ## Modify primary key.
 ## Do this for all DBs, create a master CSV
-indir <- "."
-outdir <- "renamed"
-if (! dir.exists(outdir)) dir.create(outdir)
-##infiles <- list.files(path = indir, pattern = "*.accdb")
-
-## make a copy so that the task can be run again if needed
-file.copy(
-  from = infiles, to = outdir, overwrite = TRUE
-)
-## Rename as otherwise you get an error while
-## reading them in
-## This only needs to be done, so can be moved outside
-## of this file.
-outfiles <- sapply(
-  infiles, function(infile) {
-    gsub(
-      pattern = paste0("DIDE Priority Pathogens ", pathogen, " - "),
-      replacement = "",
-      x = infile
-    )
-  }
-)
-
-file.rename(
-  from = list.files(path = outdir, pattern = "*.accdb", full.names = TRUE),
-  to = paste0(outdir, "/", outfiles)
-)
-
-infiles <- list.files(path = outdir, pattern = "*.accdb", full.names = TRUE)
-names(infiles) <- infiles
-
 ## model, parameter and outbreak
 ## are all tied together using
 ## Article_ID
 primary_key_col <- "Article_ID"
 outfiles <- list(
-  "Article data - Table" = "articles.csv",
-  "Model data - Table" = "models.csv",
-  "Parameter data - Table" = "parameters.csv"
+  articles = "articles.csv",
+  models = "models.csv",
+  params = "parameters.csv"
   ## TODO add the name of outbreaks table
   ## and add a file name here,
   ## xxx = "outbreaks.csv"
@@ -81,80 +63,78 @@ outfiles <- list(
 from <- map(
   infiles, function(infile) {
     message("Reading ", infile)
-    mdb.get(infile, allow = "_")
-  }
-)
-
-## Remove databases with empty tables
-from <- keep(from, function(y) nrow(y[[1]]) > 0)
-
-## Update the ID of each table.
-## First modify the article column, adding an
-## extra column. Then get this new column tacked
-## on to the other tables as well.
-## Deal with blank extractor name first
-from <- imap(
-  from, function(x, y) {
-    message("Now reading ", y)
-    ## Append a new column
-    narticles <- nrow(x[["Article data - Table"]])
-
-
-    x[["Article data - Table"]]$ID <- random_id(
+    con <- dbConnect(drv = odbc(), .connection_string = paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=[./", infile, "];"))
+    
+    if (is.null(con)){
+      message("Error in reading ", infile)
+      return()
+    }
+    
+    res <- dbSendQuery(con, "SELECT * FROM [Article data - Table]")
+    articles <- dbFetch(res)
+    narticles <- nrow(articles)
+    
+    if (narticles == 0) return()
+    
+    articles$ID <- random_id(
       n = narticles, use_openssl = FALSE
     )
-    ## Join with other tables so that they get this
-    ## new column.
-    ## Convert to integer as this is read in as
-    ## labelled data
-    x[["Model data - Table"]][[primary_key_col]] <- as.integer(
-      x[["Model data - Table"]][[primary_key_col]]
+    
+    
+    res <- dbSendQuery(con, "SELECT * FROM [Model data - Table]")
+    models <- dbFetch(res)
+   
+    
+    models <- left_join(
+      articles[ , c("Article_ID", "ID", "Pathogen", "Covidence_ID", "Name_data_entry")],
+      models,
+      by = "Article_ID"
     )
-    x[["Model data - Table"]] <- left_join(
-      x[["Article data - Table"]][ , c(primary_key_col, "ID")],
-      x[["Model data - Table"]],
-      by = primary_key_col
-    )
-    ## update model_id column as it has no use now
-    ## better than having multiple ID columns hanging around
-    nmodels <- nrow(x[["Model data - Table"]])
-    x[["Model data - Table"]][["Model_data_ID"]] <- random_id(
+    nmodels <- nrow(models)
+    models$Model_data_ID <- random_id(
       n = nmodels, use_openssl = FALSE
     )
-    ## Same for the third table
-    x[["Parameter data - Table"]][[primary_key_col]] <- as.integer(
-      x[["Parameter data - Table"]][[primary_key_col]]
+    
+    
+    res <- dbSendQuery(con, "SELECT * FROM [Parameter data - Table]")
+    params <- dbFetch(res)
+    
+    
+    params <- left_join(
+      articles[ , c("Article_ID", "ID", "Pathogen", "Covidence_ID", "Name_data_entry")],
+      params,
+      by = "Article_ID"
     )
-    x[["Parameter data - Table"]] <- left_join(
-      x[["Article data - Table"]][ , c(primary_key_col, "ID")],
-      x[["Parameter data - Table"]],
-      by = primary_key_col
-    )
-    nparams <- nrow(x[["Parameter data - Table"]])
-    x[["Parameter data - Table"]][["Parameter_data_ID"]] <-  random_id(
+    nparams <- nrow(params)
+    params$Parameter_data_ID <- random_id(
       n = nparams, use_openssl = FALSE
     )
-    ## TODO
-    ## Do the same for outbreaks table
-    x
+    
+    # TODO Extract outbreaks table where relevant
+    list(
+      articles = articles, models = models, params = params
+    )
   }
 )
+
 ## Write each DB to the same file now.
 walk(
   from, function(x) {
-    walk2(
-      outfiles, x, function(file, df) {
-        ## TODO
-        ## have to do something about this if we
-        ## decide to commit these files to git
-        append <- FALSE
-        if (file.exists(file)) append <- TRUE
-        message(file)
-        write_csv(df, file, append = append)
+    iwalk(x, function(df, dfname) {
+      ## TODO
+      ## have to do something about this if we
+      ## decide to commit these files to git
+      file <- outfiles[[dfname]] 
+      append <- FALSE
+      if (file.exists(file)) append <- TRUE
+      message("Writing to ", file)
+      write_csv(df, file, append = append)
+    }
+    )
       }
     )
-  }
-)
+  
+
 ## Empty outbreaks.csv for Ebola
 ## Amend this for other pathogens
 file.create("outbreaks.csv")
