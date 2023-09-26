ad7f1797d480a4f40e45d577a58eebe6fdb24e93
rebeccanash <r.nash@imperial.ac.uk>
2023-09-26 17:20:43 +0100

    update orderly files

--- src/db_compilation/orderly.R
+++ src/db_compilation/orderly.R
@@ -1,6 +1,8 @@
 # Task to compile single and double extraction databases together
+library(readr)
 library(orderly2)
 library(dplyr)
+library(janitor)
 orderly_strict_mode()
 
 ## Outputs
@@ -9,61 +11,100 @@ orderly_artefact(
   c("articles.csv", "models.csv", "parameters.csv"))
 
 # # Once working as an orderly task, we can get inputs from previous tasks
-# orderly_dependency("db_extraction", "TASKID",
+# orderly_dependency("db_extraction", "20230914-083210-af90dec1",
 #   c("single_extraction_articles.csv" = "single_extraction_articles.csv",
-#     "single_extraction_parameters.csv" = "single_extraction_parameters.csv",
-#     "single_extraction_models.csv" = "single_extraction_models.csv")
+#     "single_extraction_params.csv" = "single_extraction_params.csv",
+#     "single_extraction_models.csv" = "single_extraction_models.csv",
+#     "double_extraction_articles.csv" = "double_extraction_articles.csv")
 # )
 # 
-# orderly_dependency("db_double", "TASKID",
-#                    c("qa_match.csv" = "qa_match.csv")
+# The below task also produces the fixing files that need to be manually changed
+# orderly_dependency("db_double", "20230914-085025-6f8c5333",
+#                    c("qa_matching.csv" = "qa_matching.csv")
 # )
 
 ## Inputs (when set up properly, this will just list the manually fixed files)
 orderly_resource(
   c("single_extraction_articles.csv",
-    "single_extraction_parameters.csv",
+    "single_extraction_params.csv",
     "single_extraction_models.csv",
+    "double_extraction_articles.csv",
+    "qa_matching.csv",
     "qa_fixing.csv",
-    "parameter_fixing.csv",
-    "model_fixing.csv",
-    "qa_match.csv"
+    "params_fixing.csv",
+    "models_fixing.csv",
+    "cleaning.R"
   )
 )
 
+source("cleaning.R")
+
 # Single extractions
-article_single <- import("articles.csv")
-model_single <- import("models.csv")
-parameter_single <- import("parameters.csv")
+article_single <- read_csv("single_extraction_articles.csv")
+model_single <- read_csv("single_extraction_models.csv")
+parameter_single <- read_csv("single_extraction_params.csv")
 outbreak_single <- NULL
 
+article_single <- article_single %>% clean_names()
+
+model_single <- model_single %>% clean_names()
+
+parameter_single <- parameter_single %>% clean_names()
+
 # Double extractions - matched between extractors
-qa_matching <- import("qa_match.csv")
+article_double <- read_csv("double_extraction_articles.csv")
+qa_matching <- read_csv("qa_matching.csv")
 model_matching <- NULL
 parameter_matching <- NULL
 outbreak_matching <- NULL
 
+article_double <- article_double %>% clean_names() %>% arrange(covidence_id)
+
+qa_matching <- qa_matching %>% clean_names()
+
 # Double extractions - needed to be resolved between extractors
-qa_fixed <- import("qa_fixing.csv")
-model_fixed <- import("model_fixing.csv")
-parameter_fixed <- import("parameter_fixing.csv")
+qa_fixed <- read_csv("qa_fixing.csv")
+model_fixed <- read_csv("models_fixing.csv")
+parameter_fixed <- read_csv("params_fixing.csv")
 outbreak_fixed <- NULL
 
 ## create final datasets
 # TO DO: Add outbreak_fixed for next pathogen
 qa_fixed <- qa_fixed %>%
-  dplyr::filter(fixed == TRUE) %>%
-  dplyr::select(names(article_single))
+  filter(fixed == TRUE) %>%
+  select(names(qa_matching))
 
 parameter_fixed <- parameter_fixed %>%
-  dplyr::filter(fixed == TRUE) %>%
-  dplyr::select(names(parameter_single))
+  filter(fixed == TRUE) %>%
+  select(names(parameter_single))
 
 model_fixed <- model_fixed %>%
-  dplyr::filter(fixed == TRUE) %>%
-  dplyr::select(names(model_single))
+  filter(fixed == TRUE) %>%
+  select(names(model_single))
 
-#article_final <- 
+# join article data to qa files
+article_double_details <- article_double %>% select(-c(starts_with("qa")))
+
+article_matching <- qa_matching %>%
+  select(-c("num_rows", "matching")) %>%
+  left_join(article_double_details,
+            by = c("id", "covidence_id", "name_data_entry")) %>%
+  distinct(covidence_id, .keep_all = TRUE) %>%
+  mutate(double_extracted = 1)
+
+article_fixed <- qa_fixed %>%
+  select(-c("num_rows", "matching")) %>%
+  left_join(article_double_details,
+            by = c("id", "covidence_id", "name_data_entry")) %>%
+  mutate(double_extracted = 1)
+
+article_single <- article_single %>%
+  mutate(double_extracted = 0)
+
+# bind single and double together
+article_all <- rbind(article_single,
+                     article_matching,
+                     article_fixed)
 
 parameter_all <- rbind(parameter_single,
                        parameter_matching,
@@ -72,6 +113,10 @@ parameter_all <- rbind(parameter_single,
 model_all <- rbind(model_single,
                    model_matching,
                    model_fixed)
+
+# Cleaning
+parameter_all <- clean_dfs(parameter_all, "parameter_type")
+model_all <- clean_dfs(model_all, "model_type")
 
 if (! dir.exists(outdir)) dir.create(outdir)
 write_csv(parameter_all, "outdir/parameters.csv")
