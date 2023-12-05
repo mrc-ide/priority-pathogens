a9e3fc0fe35ce95d67d2beabf19c18a7c4bba3b7
rebeccanash <r.nash@imperial.ac.uk>
2023-12-05 15:33:06 +0000

    Edit reproduction number task with shared resources and more plots

--- src/ebola_reproduction_number/orderly.R
+++ src/ebola_reproduction_number/orderly.R
@@ -17,14 +17,14 @@ orderly_strict_mode()
 orderly_artefact(
   "Plots and tables for reproduction number parameters",
   c(
-    "R_plots/basic_r_plot_filtered.png",
-    "R_plots/eff_r_plot_filtered.png",
-    
+    "R_plots/basic_r_outbreak_filtered.png",
+    "R_plots/basic_r_country_filtered.png",
+    "R_plots/eff_r_outbreak_filtered.png",
+    "R_plots/eff_r_country_filtered.png",
     "R_tables/qa_filtered/basic_r_tab_filtered.png",
     "R_tables/qa_filtered/eff_r_tab_filtered.png",
     "R_tables/unfiltered/basic_r_tab_all.png",
     "R_tables/unfiltered/eff_r_tab_all.png",
-    
     "R_tables/qa_filtered/range_outbreak.png",
     "R_tables/qa_filtered/range_country.png",
     "R_tables/qa_filtered/range_species.png"
@@ -45,12 +45,14 @@ orderly_dependency(
 )
 
 # Script with functions for plots/tables
-orderly_resource("functions.R")
+orderly_shared_resource("ebola_functions.R" = "ebola_functions.R")
+orderly_shared_resource("ebola_visualisation.R" = "ebola_visualisation.R")
 
 # Load data
 articles <- read_csv("articles.csv")
 params <- read_csv("parameters.csv")
-source("functions.R")
+source("ebola_functions.R")
+source("ebola_visualisation.R")
 
 parameter <- "Reproduction number"
 
@@ -62,7 +64,9 @@ df <- left_join(
   arrange(article_label, -year_publication)
 
 species_levels <- factor(df$ebola_species, levels = c(sort(
-  setdiff(unique(df$ebola_species), "Unspecified"), decreasing = TRUE), "Unspecified"))
+  setdiff(unique(df$ebola_species), "Unspecified"),
+  decreasing = TRUE
+), "Unspecified"))
 
 df_plot <- df %>%
   filter(parameter_class == parameter) %>%
@@ -77,25 +81,11 @@ df_plot <- df %>%
         )
       ),
     article_label_unique = make.unique(article_label),
-    outbreak = factor(outbreak, levels = c(
-      "DRC, 1976",
-      "South Sudan, 1976",
-      "DRC & South Sudan, 1976",
-      "DRC, 1995",
-      "Uganda, 2000-2001",
-      "Uganda, 2007",
-      "DRC, 2008-2009",
-      "DRC, 2012",
-      "West Africa 2013-2016",
-      "DRC, 2014",
-      "DRC, 2018-2020",
-      "Guinea, 2021",
-      "Uganda, 2022",
-      "Unspecified"
-    )),
+    outbreak = order_ebola_outbreaks(outbreak),
     ebola_species = factor(ebola_species, levels = c(
       sort(setdiff(unique(ebola_species), "Unspecified"), decreasing = FALSE),
-      "Unspecified")),
+      "Unspecified"
+    )),
     population_study_start_month =
       factor(population_study_start_month,
         levels = c(
@@ -110,8 +100,8 @@ ordered_dat <- df_plot %>%
   mutate(
     range_midpoint =
       ifelse(is.na(parameter_value) & !is.na(parameter_upper_bound),
-             parameter_upper_bound - parameter_lower_bound, NA
-    ),
+        parameter_upper_bound - parameter_lower_bound, NA
+      ),
     temp_order_by = ifelse(!is.na(parameter_value),
       parameter_value,
       range_midpoint
@@ -135,26 +125,56 @@ dir.create("R_tables/unfiltered")
 # PLOTS
 
 # Plots with qa_filter of >=50
-basic_r_plot_qa <- create_plot(
+basic_r_outbreak_qa <- create_plot(
   ordered_dat,
   param = parameter,
   r_type = "Basic (R0)",
-  qa_filter = TRUE
+  qa_filter = TRUE,
+  facet_by = "outbreak",
+  symbol_col_by = "population_country"
 )
 
-eff_r_plot_qa <- create_plot(
+basic_r_country_qa <- create_plot(
+  ordered_dat,
+  param = parameter,
+  r_type = "Basic (R0)",
+  qa_filter = TRUE,
+  facet_by = "population_country",
+  symbol_col_by = "outbreak"
+)
+
+eff_r_outbreak_qa <- create_plot(
   ordered_dat,
   param = parameter,
   r_type = "Effective (Re)",
-  qa_filter = TRUE
+  qa_filter = TRUE,
+  facet_by = "outbreak",
+  symbol_col_by = "population_country"
+)
+
+eff_r_country_qa <- create_plot(
+  ordered_dat,
+  param = parameter,
+  r_type = "Effective (Re)",
+  qa_filter = TRUE,
+  facet_by = "population_country",
+  symbol_col_by = "outbreak"
 )
 
 # Save
-ggsave("R_plots/basic_r_plot_filtered.png", basic_r_plot_qa,
+ggsave("R_plots/basic_r_outbreak_filtered.png", basic_r_outbreak_qa,
   width = 8, height = 11, units = "in", bg = "white"
 )
 
-ggsave("R_plots/eff_r_plot_filtered.png", eff_r_plot_qa,
+ggsave("R_plots/basic_r_country_filtered.png", basic_r_country_qa,
+  width = 8, height = 11, units = "in", bg = "white"
+)
+
+ggsave("R_plots/eff_r_outbreak_filtered.png", eff_r_outbreak_qa,
+  width = 8, height = 5, units = "in", bg = "white"
+)
+
+ggsave("R_plots/eff_r_country_filtered.png", eff_r_country_qa,
   width = 8, height = 5, units = "in", bg = "white"
 )
 
@@ -166,27 +186,29 @@ basic_r_tab_qa <- create_table(
   param = parameter,
   r_type = "Basic (R0)",
   qa_filter = TRUE
-  )
+)
 
 eff_r_tab_qa <- create_table(
   ordered_dat,
   param = parameter,
   r_type = "Effective (Re)",
   qa_filter = TRUE
-  )
+)
 
 # Tables with no qa_filter
 basic_r_tab <- create_table(
   ordered_dat,
   param = parameter,
   r_type = "Basic (R0)",
-  qa_filter = FALSE)
+  qa_filter = FALSE
+)
 
 eff_r_tab <- create_table(
   ordered_dat,
   param = parameter,
   r_type = "Effective (Re)",
-  qa_filter = FALSE)
+  qa_filter = FALSE
+)
 
 # Save
 save_as_image(basic_r_tab_qa, path = "R_tables/qa_filtered/basic_r_tab_filtered.png")
@@ -206,26 +228,29 @@ range_dat <- ordered_dat %>%
   mutate(
     parameter_value_type =
       case_when(parameter_value_type %in% c("Unspecified", "Other")
-      ~ "Other_Unspecified", TRUE ~ parameter_value_type)
+      ~ "Other/Unspecified", TRUE ~ parameter_value_type)
   )
 
 range_outbreak <- create_range_table(
   df = range_dat,
-  group_tab = "Outbreak",
-  value_type = c("Mean", "Median", "Other_Unspecified"),
-  qa_filter = TRUE)
+  main_group = "outbreak", main_group_label = "Outbreak",
+  sub_group = "parameter_value_type", sub_group_label = "Estimate Type",
+  qa_filter = TRUE
+)
 
 range_country <- create_range_table(
   df = range_dat,
-  group_tab = "Country",
-  value_type = c("Mean", "Median", "Other_Unspecified"),
-  qa_filter = TRUE)
+  main_group = "population_country", main_group_label = "Country",
+  sub_group = "parameter_value_type", sub_group_label = "Estimate Type",
+  qa_filter = TRUE
+)
 
 range_species <- create_range_table(
   df = range_dat,
-  group_tab = "Species",
-  value_type = c("Mean", "Median", "Other_Unspecified"),
-  qa_filter = TRUE)
+  main_group = "ebola_species", main_group_label = "Species",
+  sub_group = "parameter_value_type", sub_group_label = "Estimate Type",
+  qa_filter = TRUE
+)
 
 # Save
 save_as_image(range_outbreak, path = "R_tables/qa_filtered/range_outbreak.png")
