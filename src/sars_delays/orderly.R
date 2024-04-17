e3fdf64661f9f76e782c7a59015b4b5c55af1777
cm401 <christian.morgenstern@gmail.com>
2024-04-17 19:02:47 +0100

    initial revision (placeholder for analysis)

---
+++ src/sars_delays/orderly.R
@@ -0,0 +1,67 @@
+#task to create sars latex tables
+
+library(tidyverse)
+library(stringr)
+library(metafor)
+library(meta)
+library(estmeansd)
+library(mixdist)
+library(ggplot2)
+library(ggsci)
+library(sf)
+library(ragg)
+library(ggspatial)
+library(ggforce)
+library(png)
+library(grid)
+library(patchwork)
+library(gridExtra)
+
+#orderly preparation 
+orderly_strict_mode()
+orderly_parameters(pathogen = NULL)
+orderly_dependency("db_compilation", "latest(parameter:pathogen == this:pathogen)",
+                   c("articles.csv", "models.csv", "parameters.csv"))
+orderly_shared_resource("lassa_functions.R" = "lassa_functions.R")
+source("lassa_functions.R")
+orderly_artefact("sars-specific tables",c("figure_delays.png"))
+
+###################
+## DATA CURATION ##
+###################
+
+articles   <- read_csv("articles.csv")
+models     <- read_csv("models.csv")
+parameters <- read_csv("parameters.csv")
+
+dfs <- curation(articles,tibble(),models,parameters, adjust_for_exponents = FALSE )
+
+articles   <- dfs$articles
+models     <- dfs$models
+parameters <- dfs$parameters
+
+
+d1 <- parameters %>% filter(parameter_type == 'Human delay - incubation period')
+d2 <- parameters %>% filter(parameter_type == 'Human delay - symptom onset>admission to care')
+d3 <- parameters %>% filter(parameter_type == 'Human delay - time in care (length of stay)' |
+                              parameter_type == 'Human delay - admission to care>discharge/recovery' |
+                              parameter_type == 'Human delay - admission to care>death')
+d4 <- parameters %>% filter(parameter_type == 'Human delay - symptom onset>discharge/recovery' |
+                              parameter_type == 'Human delay - symptom onset>death')
+d5 <- parameters %>% filter(parameter_type == 'Human delay - serial interval')
+
+p1 <- forest_plot(d1,'Incubation Period (days)',"parameter_type",c(0,40))
+
+p2 <- forest_plot(d5,'Serial Interval (days)',"parameter_type",c(0,20))
+
+
+
+#meta-analysis of onset-admission delay
+set.seed(42)
+m5 <- metamean_wrap(dataframe = d2, estmeansd_method = "Cai",
+                    plot_study = TRUE, digits = 2, lims = c(0,6), colour = "dodgerblue3", label = "Mean Onset-Admission Delay (days)",
+                    width = 9500, height = 4500, resolution = 1000)
+
+p5 <- m5$plot
+
+ggsave("figure_delays.png", plot = p5, width = 12, height = 16)
