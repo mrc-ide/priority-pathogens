bfbb5120d93c0455a514975f9c001ac2dcb6dc41
cm401 <christian.morgenstern@gmail.com>
2024-06-24 19:00:47 +0100

    Add some more analysis

--- src/sars_delays/orderly.R
+++ src/sars_delays/orderly.R
@@ -33,12 +33,14 @@ models     <- read_csv("models.csv")
 parameters <- read_csv("parameters.csv")
 
 #dfs <- curation(articles,tibble(),models,parameters, adjust_for_exponents = FALSE )
-dfs <- data_curation(articles,tibble(),models,parameters, plotting = FALSE )
+dfs <- data_curation(articles,tibble(),models,parameters, plotting = TRUE )
 
 articles   <- dfs$articles
-models     <- dfs$models
-parameters <- dfs$parameters
+articles   <- epireview::assign_qa_score(articles = articles)$articles
+qa_scores  <- articles %>% dplyr::select(covidence_id,qa_score)
 
+models     <- dfs$models
+parameters <- dfs$parameters %>% left_join(qa_scores) 
 
 # sample sd is the same as sd, so need to make this clear somehow / change param field to work for meta analysis
 
@@ -46,26 +48,54 @@ d1 <- parameters %>% filter(parameter_type == 'Human delay - incubation period')
 d2 <- parameters %>% filter(parameter_type == 'Human delay - symptom onset>admission to care')
 d3 <- parameters %>% filter(parameter_type == 'Human delay - time in care (length of stay)' |
                               parameter_type == 'Human delay - admission to care>discharge/recovery' |
-                              parameter_type == 'Human delay - admission to care>death')
+                              parameter_type == 'Human delay - admission to care>death') %>%
+  mutate(parameter_value_type = case_when(is.na(parameter_value_type)~'Unspecified',
+                                    TRUE ~ parameter_value_type))
 d4 <- parameters %>% filter(parameter_type == 'Human delay - symptom onset>discharge/recovery' |
                               parameter_type == 'Human delay - symptom onset>death')
 d5 <- parameters %>% filter(parameter_type == 'Human delay - serial interval')
 
-p1 <- forest_plot(d1,'Incubation Period (days)',"parameter_type",c(0,30))
+# Serial interval sub-plot (both papers have qa above 0.5)
+SI_forest <- forest_plot(d5 %>% filter(qa_score>0.5),'Serial Interval (days)',"parameter_type",c(0,20))
 
-#d1$article_label <- d1$refs
-#p1_epireview <- epireview::forest_plot_incubation_period(d1,30,FALSE)
-
-p2 <- forest_plot(d5,'Serial Interval (days)',"parameter_type",c(0,20))
-
-
+# Incubation period
+#p1 <- forest_plot(d1,'Incubation Period (days)',"parameter_type",c(0,30))
+set.seed(42)
+d1_subgroups <- d1 %>% filter(population_group %in% c('General population','Mixed groups', 'Persons under investigation','Healthcare workers')) %>%
+  filter(qa_score>0.5) %>%
+  mutate(parameter_value = coalesce(parameter_value,central))
+m1 <- metamean_wrap(dataframe = d1_subgroups, estmeansd_method = "Cai", 
+                    plot_study = FALSE, digits = 2, lims = c(0,10), colour = "dodgerblue3", label = "Incubation Period",
+                    width = 7500, height = 5750, resolution = 1000, subgroup = 'population_group', sort_by_subg = TRUE )
 
 #meta-analysis of onset-admission delay
 set.seed(42)
-m5 <- metamean_wrap(dataframe = d2, estmeansd_method = "Cai",
-                    plot_study = TRUE, digits = 2, lims = c(0,6), colour = "dodgerblue3", label = "Mean Onset-Admission Delay (days)",
-                    width = 9500, height = 4500, resolution = 1000)
+d2 <- d2 %>% mutate(parameter_value = coalesce(parameter_value,central)) %>% arrange(desc(parameter_value)) %>%
+  filter(!is.na(parameter_uncertainty_single_value)|!is.na(parameter_uncertainty_type) ) %>%
+  filter(qa_score>0.5)
+m2 <- metamean_wrap(dataframe = d2, estmeansd_method = "Cai",
+                    plot_study = TRUE, digits = 2, lims = c(0,10), colour = "dodgerblue3", label = "Mean Onset-Admission Delay (days)",
+                    width = 9500, height = 4200, resolution = 1000)
 
-#p5 <- m5$plot
+# admission to outcome... 
+d3_subgroup <- d3 %>% 
+  filter(!is.na(parameter_uncertainty_single_value)|!is.na(parameter_uncertainty_type) ) %>% 
+  mutate(group = stringr::str_to_title(str_replace(parameter_type,'Human delay - ','')),
+         parameter_type = 'Human delay') %>%  
+  filter(qa_score>0.5)
 
-ggsave("figure_delays.png", plot = m5$plot, width = 12, height = 16)
+outcome_forest <- forest_plot(d3 %>% filter(qa_score>0.5),'Admission to outcome',"parameter_type",c(0,50))
+
+
+m3 <- metamean_wrap(dataframe = d3_subgroup, estmeansd_method = "Cai", 
+                    plot_study = FALSE, digits = 2, lims = c(8,25), colour = "dodgerblue3", label = "Admission to outcome",
+                    width = 9500, height = 4500, resolution = 1000, subgroup = 'group', sort_by_subg = TRUE )
+
+layout <- "
+AABBB
+CCDDD
+CCDDD
+"
+delays_plot <-  SI_forest + outcome_forest + m1$plot + m2$plot  + plot_layout(design = layout) + plot_annotation(tag_levels = 'A')
+
+ggsave("figure_delays.png", plot = delays_plot, width = 28, height = 18)
