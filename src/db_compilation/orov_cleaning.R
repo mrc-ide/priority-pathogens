bc21936fffc357c053a3f1ca0e1530ed02543748
ruthmccabe <ruth.mccabe17@imperial.ac.uk>
2025-02-11 10:43:57 +0000

    orov article cleaning

--- src/db_compilation/orov_cleaning.R
+++ src/db_compilation/orov_cleaning.R
@@ -1,7 +1,13 @@
-orov_cleaning <- function(df) {
-  df <- df %>% mutate(
-    
-    
-    
-  )
+orov_cleaning_articles <- function(df) {
+  df <- df %>% 
+    # dummy entry but marked as OROV 
+    filter(covidence_id!="999999",
+           # duplicate entries for 30 and 109
+           id!="6e85a97a-c427-4eef-b0e8-f55142802ef1",
+           id!="23d7b6a2-bbbe-4f94-984f-da9fd0bab2d5")
+}
+
+
+orov_cleaning_parameters <- function(df){
+  df <- df %>% mutate()
 }
