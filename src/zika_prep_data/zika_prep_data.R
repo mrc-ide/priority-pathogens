8be18fdaaa2919b610effd2b71b1798e3d872a41
Kelly McCain <kem22@dide.local>
2025-02-27 11:50:13 +0000

    add prep data and risk factors tasks

---
+++ src/zika_prep_data/zika_prep_data.R
@@ -0,0 +1,6 @@
+# This script is to be run after zika_compilation which provides basic cleaning 
+# In this script, we will do general preparation of data for tables and figures 
+# It will take the place of the data_curation() function currently housed in lassa_function.R
+
+
+
