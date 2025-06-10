8c203cb658baad5048b2eae46d832e87cb143ff7
tristan-myles <mylesnaidoo95@gmail.com>
2025-06-10 18:27:04 +0100

    Updates db_extraction prep to use strict_mode
    - In line with most other tasks in this repo

--- src/db_extraction_prep/db_extraction_prep.R
+++ src/db_extraction_prep/db_extraction_prep.R
@@ -10,6 +10,8 @@ mode <- toupper(mode)
 orderly_artefact(description="rds file with the processed tables",
                  "extracted_tables.rds")
 
+orderly_strict_mode()
+
 # Mode serves to overwrite the default pathogen method of extraction
 # E.g. if new EBOLA articles are extracted with redcap, mode can be used to
 # overwrite the default extraction method. For each pathogen, the necessary data
@@ -17,8 +19,10 @@ orderly_artefact(description="rds file with the processed tables",
 # If you use a different method, you need to provide the necessary files.
 if (pathogen %in% c("MARBURG", "EBOLA", "SARS", "ZIKA") & mode!="REDCAP"|
     mode=="ACCESS"){
+  orderly_resource("prepare_access.R")
   source("prepare_access.R")
 } else if (pathogen %in% c("OROV", "NIPAH") & mode!="ACCESS"|
            mode=="REDCAP"){
+  orderly_resource("prepare_redcap.R")
   source("prepare_redcap.R")
 }
