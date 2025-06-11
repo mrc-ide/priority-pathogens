cdda915a1e5e2a9927f3b0807f762e8f472304f2
tristan-myles <mylesnaidoo95@gmail.com>
2025-06-11 13:21:51 +0100

    Adds article cleaning
    - Sets Not Applicable qa scores to NA so that epireview assign_qa_score
      works
    - Adds journal title cleaning code that was mistakenly removed
    - Updates first_author_surname to title case

--- src/db_cleaning/nipah/nipah_cleaning.R
+++ src/db_cleaning/nipah/nipah_cleaning.R
@@ -1,5 +1,17 @@
 # *========================== Manual Niaph cleaning ===========================*
 article_cleaning <- function(df){
+  # Update Not Applicable so that epireview assign_qa_score works
+  qa_cols <- c("qa_m1", "qa_m2", "qa_a3", "qa_a4", "qa_d5", "qa_d6", "qa_d7")
+  df <- df |>
+    mutate(across(all_of(qa_cols), ~ ifelse(.=="Not Applicable", NA, .)))
+
+  # Leave first_author_first_name as is incase there are initials as the first
+  # name
+  df <- df |>
+    mutate(journal = str_to_title(journal),
+           first_author_surname = str_to_title(first_author_surname)) |>
+    mutate(journal = sub("^The\\s+", "", journal, useBytes = TRUE))
+
   return (df)
 }
 
