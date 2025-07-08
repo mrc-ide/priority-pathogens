7478fa75436e18900f33ada9afc10cd79a47dfcf
Kelly McCain <kem22@dide.local>
2025-07-08 16:29:49 +0100

    add facet_by_trimester

--- shared/zika_functions.R
+++ shared/zika_functions.R
@@ -1,13 +1,13 @@
 # function to produce forest plot for given dataframe - modified from lassa_functions.R
 
-forest_plot <- function(df, ycol = "urefs", label, shape_column = 'parameter_value_type', 
-                        color_column, lims, text_size = 12, show_label = FALSE, 
-                        custom_colours = NA, 
-                        facet_by_country = FALSE, facet_by_continent = FALSE, facet_by_poptype = FALSE, facet_by_delay = FALSE,
+forest_plot <- function(df, ycol = "urefs", label, shape_column = 'parameter_value_type',
+                        color_column, lims, text_size = 12, show_label = FALSE,
+                        custom_colours = NA,
+                        facet_by_country = FALSE, facet_by_continent = FALSE, facet_by_poptype = FALSE, facet_by_delay = FALSE,facet_by_trimester = FALSE,
                         point_size = 3) {
-  
+
   stopifnot(length(unique(df$parameter_unit[!is.na(df$parameter_unit)])) == 1)#values must have same units
-  
+
   df   <- df %>% mutate(urefs = make.unique(refs)) %>%
     mutate(urefs = factor(urefs, levels = rev(unique(urefs))),
            country_brazil = case_when(
@@ -31,7 +31,8 @@ forest_plot <- function(df, ycol = "urefs", label, shape_column = 'parameter_val
              population_country == "Singapore" | population_location == 'Singapore' ~ "Asia",
              population_country == 'Brazil' & population_location != 'Brazil' ~ "Brazil (Americas)",
              # population_country == 'French Polynesia' & population_location != 'French Polynesia' ~ "French Polynesia (Oceania)",
-             population_country == 'France' & grepl('French Polynesia', population_location) & population_location != 'French Polynesia' ~ "French Polynesia, France (Oceania)",
+             population_country == 'France' & grepl('French Polynesia', population_location) & population_location != 'French Polynesia' &
+               population_location != "French Polynesia and French West Indies" ~ "French Polynesia, France (Oceania)",
              population_location == 'French Polynesia' ~ "Oceania",
              population_location == 'Sous-le-vent Islands' ~ "French Polynesia, France (Oceania)",
              population_country == 'Colombia' & population_location != "Colombia" ~ "Colombia (Americas)",
@@ -39,17 +40,18 @@ forest_plot <- function(df, ycol = "urefs", label, shape_column = 'parameter_val
              population_country == 'France' & population_location == 'French Guiana' ~ "Americas",
              population_country == 'Unspecified' ~ "Unspecified",
              population_location == 'Veterans Health Administration in US and Caribbean' ~ "Americas",
+             population_location == "French Polynesia and French West Indies" ~ 'Multiple continents',
              TRUE ~ continent
              ),
-           continent = factor(continent, levels = c('Brazil (Americas)','French Polynesia, France (Oceania)','Colombia (Americas)', 'Americas','Asia','Europe','Oceania','Africa',"Unspecified")),
+           continent = factor(continent, levels = c('Brazil (Americas)','French Polynesia, France (Oceania)','Colombia (Americas)', 'Americas','Asia','Europe','Oceania','Africa',"Multiple continents","Unspecified")),
            population_country = str_replace(population_country, 'Multi-country: ', ""),
            label_group = case_when(
              is.na(population_location) | population_location == '' ~ population_country,
              is.na(population_country) | population_country=='' ~ population_location,
-             (!is.na(population_location) & population_country == 'Unspecified') | 
-               ((population_country == 'Brazil' | population_country == "Colombia" | continent == 'French Polynesia, France (Oceania)') & 
+             (!is.na(population_location) & population_country == 'Unspecified') |
+               ((population_country == 'Brazil' | population_country == "Colombia" | continent == 'French Polynesia, France (Oceania)') &
                   (facet_by_country == TRUE | facet_by_continent == TRUE)) ~ population_location,
-             (population_location != "" & !is.na(population_location)) & 
+             (population_location != "" & !is.na(population_location)) &
                (population_country != 'Unspecified') ~ paste0(population_location,'\n' ,' (',population_country,')'),
              TRUE ~ NA),
            label_group = case_when(
@@ -59,21 +61,21 @@ forest_plot <- function(df, ycol = "urefs", label, shape_column = 'parameter_val
              TRUE ~ label_group),
            label_group = str_replace(label_group, ", French Polynesia$", "")
            ) %>%
-    arrange(desc(parameter_value)) %>% 
+    arrange(desc(parameter_value)) %>%
     mutate(label_group = factor(label_group, levels = unique(label_group)),
            urefs = factor(urefs, levels = unique(urefs)))
   cats <- length(unique(df[[color_column]]))
-  
+
   jitterer <- position_jitter(seed = 123, width = 0.025)
-  
+
   df <- df %>%
       group_by(.data[[ycol]]) %>%
-      mutate(n = n()) 
-  
+      mutate(n = n())
+
   sortby <- if(ycol == 'label_group') 'population_country' else 'parameter_value'
-  
+
   gg <- ggplot(df) +
-    
+
     geom_segment(data = df %>% filter(n > 1) %>% arrange(sortby) %>%
                    mutate(label_group = factor(label_group, levels=unique(label_group)),
                           urefs = factor(urefs, levels = unique(urefs))),
@@ -119,136 +121,139 @@ forest_plot <- function(df, ycol = "urefs", label, shape_column = 'parameter_val
                    shape = .data[[shape_column]], fill = .data[[color_column]],
                    color = .data[[color_column]]),
                size = point_size, stroke = 1,
-               alpha = 1) 
-  
+               alpha = 1)
+
   if(facet_by_country){
     gg <- gg +
-      ggforce::facet_col(vars(country_brazil), scales = 'free_y', space = 'free')  #.~country_brazil ncol = 1, 
+      ggforce::facet_col(vars(country_brazil), scales = 'free_y', space = 'free')  #.~country_brazil ncol = 1,
   } else if(facet_by_continent){
-    gg <- gg + 
+    gg <- gg +
       ggforce::facet_col(vars(continent), scales = 'free_y', space = 'free')
   } else if(facet_by_poptype){
-    gg <- gg + 
+    gg <- gg +
       ggforce::facet_col(vars(population_sample_type), scales = 'free_y', space = 'free')
   } else if (facet_by_delay){
     gg <- gg +
       ggforce::facet_col(vars(parameter_type), scales = 'free_y', space = 'free')
+  } else if (facet_by_trimester){
+    gg <- gg +
+      ggforce::facet_col(vars(trimester_exposed), scales = 'free_y', space = 'free')
   }
-  
-  
-  
+
+
+
   if (all(df$parameter_class=="Reproduction number")) {gg <- gg + geom_vline(xintercept = 1, linetype = "dashed", colour = "dark grey")}
-  
+
   if (shape_column == 'parameter_value_type'){
-    gg <- gg + 
+    gg <- gg +
       scale_shape_manual(name = "Parameter Type",
                          values = c(Mean = 21, Median = 22, Central = 25, "Maximum likelihood" = 23, Unspecified = 24, Other = 8),
-                         breaks = c("Mean", "Median", "Unspecified", "Central", "Maximum likelihood", "Other"), 
-                         labels = function(x) str_wrap(x, width = 18)) 
+                         breaks = c("Mean", "Median", "Unspecified", "Central", "Maximum likelihood", "Other"),
+                         labels = function(x) str_wrap(x, width = 18))
   } else if (shape_column == 'case_definition'){
-    gg <- gg + 
+    gg <- gg +
       scale_shape_manual(name = 'Case definition',
                          values = c("Confirmed" = 21, "Suspected" = 22, "Confirmed or suspected" = 25,
                                     'Other' = 13,
                                     "Unspecified" = 24),
-                         breaks = c("Confirmed", "Suspected", "Confirmed or suspected", "Other","Unspecified"), 
+                         breaks = c("Confirmed", "Suspected", "Confirmed or suspected", "Other","Unspecified"),
                          labels = function(x) str_wrap(x, width = 18))
   } else if (shape_column == 'population_sample_type'){
-    gg <- gg + 
+    gg <- gg +
       scale_shape_manual(name = 'Sample type',
                          values = c("Hospital based" = 21, "Mixed settings" = 22, "Population based" = 25,
                                     "Travel based" = 23, "Community based" = 7, 'Unspecified' = 4,
                                     "Other" = 24),
                          breaks = c("Hospital based", "Mixed settings", "Population based",
-                                    "Travel based","Community based", "Other", 'Unspecified'), 
+                                    "Travel based","Community based", "Other", 'Unspecified'),
                          labels = function(x) str_wrap(x, width = 18))
   } else if (shape_column == 'population_group'){
-    gg <- gg + 
+    gg <- gg +
       scale_shape_manual(name = 'Sample group',
                          values = c("General population" = 21, "Persons under investigation" = 22, "Blood donors" = 25,
                                     "Mixed groups" = 13, 'Pregnant women' = 7, 'Children' = 8,'Household contacts of survivors'=4,
                                     "Other" = 23, 'Unspecified' = 24),
                          breaks = c("General population", "Persons under investigation", "Blood donors",
                                     "Mixed groups", 'Pregnant women', 'Children', 'Household contacts of survivors',
-                                    "Other", 'Unspecified'), 
+                                    "Other", 'Unspecified'),
                          labels = function(x) str_wrap(x, width = 18))
   }
-  
+
   lancetcols <- c('#00468BFF', '#ED0000FF','#42B540FF','#0099B4FF', '#925E9FFF','#AD002AFF','#FDAF91FF','#ADB6B6FF','#1B1919FF')
-  
+
   if(sum(!is.na(custom_colours))) {
-      gg <- gg + 
+      gg <- gg +
         scale_color_manual(values = custom_colours, labels = function(x) str_wrap(x, width = 18)) +
-        scale_fill_manual(values = custom_colours, labels = function(x) str_wrap(x, width = 18)) 
+        scale_fill_manual(values = custom_colours, labels = function(x) str_wrap(x, width = 18))
   } else {
     if(color_column == 'population_sample_type'){
-      gg <- gg + 
+      gg <- gg +
         scale_color_manual(values = c("Population based" = lancetcols[1], 'Hospital based' =  lancetcols[2] ,
                                       "Community based"  = lancetcols[3], 'Travel based' =    lancetcols[4] ,
                                       'Unspecified'      = lancetcols[8], 'Contact based' =   lancetcols[6] ,
                                       'Mixed settings'   = lancetcols[7], 'Other' =           lancetcols[5] ,
-                                      'School based'     = lancetcols[9], 'Household based' = "#DF8F44"), 
+                                      'School based'     = lancetcols[9], 'Household based' = "#DF8F44"),
                            labels = function(x) str_wrap(x, width = 18)) +
         scale_fill_manual(values = c("Population based" = lancetcols[1], 'Hospital based' =   lancetcols[2] ,
                                      "Community based"  = lancetcols[3], 'Travel based' =     lancetcols[4] ,
                                      'Unspecified'      = lancetcols[8], 'Contact based' =    lancetcols[6] ,
                                      'Mixed settings'   = lancetcols[7], 'Other' =            lancetcols[5] ,
-                                     'School based'     = lancetcols[9], 'Household based' =  "#DF8F44"), 
-                          labels = function(x) str_wrap(x, width = 18)) 
+                                     'School based'     = lancetcols[9], 'Household based' =  "#DF8F44"),
+                          labels = function(x) str_wrap(x, width = 18))
     } else if(color_column == 'parameter_type'){
-      gg <- gg + 
+      gg <- gg +
         scale_color_manual(values = c("Serial interval" = lancetcols[5], 'Symptom onset to admission' =  lancetcols[6] ,
                                       "Symptom onset to recovery"  = lancetcols[7], "Latent period" = "#DF8F44",
-                                      "Admission to care to discharge from care" = lancetcols[4]), 
+                                      "Admission to care to discharge from care" = lancetcols[4]),
                            labels = function(x) str_wrap(x, width = 18)) +
         scale_fill_manual(values = c("Serial interval" = lancetcols[5], 'Symptom onset to admission' =  lancetcols[6] ,
                                      "Symptom onset to recovery"  = lancetcols[7], "Latent period" = "#DF8F44",
-                                     "Admission to care to discharge from care" = lancetcols[4]), 
-                          labels = function(x) str_wrap(x, width = 18)) 
+                                     "Admission to care to discharge from care" = lancetcols[4]),
+                          labels = function(x) str_wrap(x, width = 18))
     } else if(color_column == 'population_group'){
-      gg <- gg + 
+      gg <- gg +
         scale_color_manual(values = c("General population" = lancetcols[1], 'Persons under investigation' =  lancetcols[2] ,
                                       "Other"  = lancetcols[3], 'Children' =    lancetcols[4] ,
                                       'Pregnant women'      = lancetcols[5], 'Mixed groups' =   lancetcols[6] ,
                                       'Blood donors'   = lancetcols[7], 'Unspecified' =  lancetcols[8] ,
-                                      'Household contacts of survivors' = "#DF8F44"), 
+                                      'Household contacts of survivors' = "#DF8F44"),
                            labels = function(x) str_wrap(x, width = 18)) +
         scale_fill_manual(values = c("General population" = lancetcols[1], 'Persons under investigation' =  lancetcols[2] ,
                                      "Other"  = lancetcols[3], 'Children' =    lancetcols[4] ,
                                      'Pregnant women'      = lancetcols[5], 'Mixed groups' =   lancetcols[6] ,
                                      'Blood donors'   = lancetcols[7], 'Unspecified' =  lancetcols[8] ,
-                                     'Household contacts of survivors' = "#DF8F44"), 
-                          labels = function(x) str_wrap(x, width = 18)) 
-    } else {gg <- gg + 
+                                     'Household contacts of survivors' = "#DF8F44"),
+                          labels = function(x) str_wrap(x, width = 18))
+    } else {gg <- gg +
       scale_fill_lancet(palette = "lanonc", labels = function(x) str_wrap(x, width = 18)) +
-      scale_color_lancet(palette = "lanonc", labels = function(x) str_wrap(x, width = 18)) 
+      scale_color_lancet(palette = "lanonc", labels = function(x) str_wrap(x, width = 18))
     }
   }
-  
+
   gg <- gg +
     scale_x_continuous(limits = lims, expand = c(0.01, 0)) +
     labs(x = label, y = NULL) +
-    theme_minimal() + 
+    theme_minimal() +
     theme(panel.border = element_rect(color = "black", linewidth = 1.25, fill = NA),
           text = element_text(size = text_size))#,
-          # legend.key.size = unit(5, 'cm')) 
-    
+          # legend.key.size = unit(5, 'cm'))
+
   if(ycol == 'urefs'){
-    gg <- gg + 
+    gg <- gg +
       scale_y_discrete(labels = setNames(df$refs, df$urefs))
   } else {
-    gg <- gg + 
-      scale_y_discrete(labels = function(x) str_wrap(x, width = 30)) 
+    gg <- gg +
+      scale_y_discrete(labels = function(x) str_wrap(x, width = 30))
   }
-  
+
   # if (cats == 1) {
   #   gg <- gg + guides(fill = "none", color = FALSE, shape = guide_legend(title = NULL,order = 1))
   # } else {
     gg <- gg + guides(fill = "none", color = guide_legend(title = NULL,order = 1), shape = guide_legend(title = NULL,order = 2))#}
-  
+
   if(show_label)
-    gg <- gg + geom_text_repel(aes(x = coalesce(parameter_value), y = urefs, label = population_country_ISO), nudge_y = 0.5, segment.color = "grey50" ) 
-  #gg <- gg + geom_text_repel(aes(x = coalesce(parameter_uncertainty_upper_value,parameter_upper_bound,parameter_value), y = urefs, label = population_country_ISO), nudge_x = 1.5, segment.color = "grey90" ) 
-  
+    gg <- gg + geom_text_repel(aes(x = coalesce(parameter_value), y = urefs, label = population_country_ISO), nudge_y = 0.5, segment.color = "grey50" )
+  #gg <- gg + geom_text_repel(aes(x = coalesce(parameter_uncertainty_upper_value,parameter_upper_bound,parameter_value), y = urefs, label = population_country_ISO), nudge_x = 1.5, segment.color = "grey90" )
+
   return(gg)
 }
