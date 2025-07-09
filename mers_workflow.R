5b58b78fd36ad8f49632fe23cb5c995075b110cc
tristan-myles <mylesnaidoo95@gmail.com>
2025-07-09 16:59:04 +0100

    Adds tasks up to db_compilation to MERS workflow

--- mers_workflow.R
+++ mers_workflow.R
@@ -8,3 +8,28 @@ library(orderly2)
 #     (relative path: src/db_redcap_download/download_config/mers_config.yaml)
 orderly_run("db_redcap_download",list(pathogen="MERS"))
 
+# *----------------- Prepare data to generate extraction csvs -----------------*
+# Prepares the REDCap data so that double and single extraction csvs can be
+# created in db_extraction
+# Requires:
+#   - target_table.csv (relative path: shared/redcap_data/target_table.csv)
+#   - mapping_table.csv
+#     (relative path: shared/redcap_data/mers/mapping_table.csv)
+#   - config.yaml that specifies how to run the task
+#     (relative path: src/db_extraction_prep/redcap_task/mers/config.yaml)
+orderly_run("db_extraction_prep", list(pathogen="MERS",
+                                       orderly_download_dependency=TRUE))
+
+# *------------------------- Generate extraction csvs -------------------------*
+# Extracts double and single extraction csvs used the .rds file from
+# db_extraction_prep
+orderly_run("db_extraction",list(pathogen="MERS"))
+
+# *-------------------------- Generate fixing files ---------------------------*
+# Check if any double extractions need to be fixed and generate necessary files
+orderly_run("db_double",list(pathogen="MERS"))
+
+# *-------------------------- Generate final dataset --------------------------*
+# Merge double_extraction, fixing, and single extraction files to create a final
+# dataset
+orderly_run("db_compilation", list(pathogen="MERS"))
