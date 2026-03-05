0e4236b93faeb47f3206c22749bc94f23a8a97cb
tristan-myles <mylesnaidoo95@gmail.com>
2026-03-05 17:42:08 +0000

    Updates incp meta to match cfr approach
    - Uses existing param data rather than declaring the data manually
    - Updates the plot format to match the CFR meta

--- src/nipah_inc_period_meta/nipah_inc_period_meta.R
+++ src/nipah_inc_period_meta/nipah_inc_period_meta.R
@@ -63,13 +63,34 @@ datasets_with_refs <- list(
 datasets <- lapply(datasets_with_refs, function(x) x[!names(x) %in% "access_param_id"])
 
 
-params_in <- as_tibble(parameters) %>%
-  filter(parameter_type == 'Human delay - incubation period' &
-         parameter_value_type == 'Median' &
-         !is.na(population_sample_size) &
-         population_sample_size < 23 )
+# params_in <- as_tibble(parameters) %>%
+#   filter(parameter_type == 'Human delay - incubation period' &
+#          parameter_value_type == 'Median' &
+#          !is.na(population_sample_size) &
+#          population_sample_size < 23 )
 
-params_in$parameter_uncertainty_type <- 'range'
+# No sample size 139_004 (although median (range))
+params_in <- parameters |>
+  filter(parameter_type == 'Human delay - incubation period',
+         !is.na(population_sample_size),
+         parameter_value_type=="Mean" &
+           grepl(x = tolower(parameter_2_value_type),
+                 pattern = "standard deviation") |
+           parameter_value_type=="Median" &
+           grepl(x = tolower(parameter_2_value_type),
+                 pattern = "iqr") |
+           parameter_value_type == "Median" &
+           grepl(x = tolower(parameter_2_value_type),
+             pattern = "range")) |>
+  mutate(duplicate_incp = case_when(
+    access_param_id %in% c("138_020", "138_3141") ~ "Known",
+    TRUE~"False"))
+
+params_in <- params_in |>
+  filter(duplicate_incp!="Known")
+
+# params_in$parameter_uncertainty_type <- 'range'
+params_in$parameter_uncertainty_type <- params_in$parameter_2_value_type
 params_in$parameter_uncertainty_lower_value <- params_in$parameter_2_lower_bound
 params_in$parameter_uncertainty_upper_value <- params_in$parameter_2_upper_bound
 
@@ -78,7 +99,7 @@ ip_same_data <-
   metamean_wrap(dataframe = params_in, estmeansd_method = "Cai",
                 plot_study = TRUE, digits = 2, lims = c(2,15),
                 colour = "dodgerblue3", label = "Median Incubation Period (days)",
-                width = 9500, height = 4200, resolution = 1000)
+                width = 11000, height = 4200, resolution = 1000)
 
 
 params_ip_all <- as_tibble(parameters) %>%
@@ -99,9 +120,44 @@ ip_all_data <- metamean_wrap(
   width = 9500, height = 4200, resolution = 1000
 )
 
+imperial_khaki <- "#EFE58B"
+
 saveRDS(ip_same_data, file = "ip_same_data.rds")
-png("nipah_ip_meta_analysis_same_data.png", width = 2800, height = 1000, res = 300)
-plot(ip_same_data$result)
+png("nipah_ip_meta_analysis_same_data.png", width = 12400, height = 4600,
+    res = 1000)
+
+# can't get size by square size right but the random effects look similar and
+# this is what we focus on
+forest(ip_same_data$result, layout = "Revman5",
+       colgap.forest.left = "3mm",
+       leftcols = c("studlab", "mean", "sd",
+                    "n", "w.common",
+                    "w.random", "effect.ci"),
+       leftlabs = c("Study", "Mean", "SD", "Total", "Weight  \n(common)",
+                    "Weight  \n(random)", ""),
+       colgap.left = "7mm",
+       smlab.pos = 4,
+       overall = TRUE, pooled.events = TRUE,
+       print.subgroup.name = FALSE, sort.subgroup = TRUE,
+       study.results = TRUE,
+       digits = 2,
+       digits.sd = 2,
+       showweights = TRUE,
+       col.diamond.lines = "black",col.diamond.common = "dodgerblue3",
+       col.diamond.random = "dodgerblue3",
+       col.square = imperial_khaki, col.square.lines = "black",
+       col.study = "black", col.subgroup = "black",
+       col.inside = "black", weight.study = "same",
+       xlim = c(2,12),
+       xlab="Incubation period (days)",
+       fs.predict.labels = 11.5,
+       fs.hetstat=11,
+       fs.test.subgroup = 11,
+       fs.axis = 11,
+       fontsize = 14,
+       plotwidth = "72.5mm",
+       spacing = 1.15)
+
 dev.off()
 
 orderly_artefact(files = c("ip_same_data.rds", "nipah_ip_meta_analysis_same_data.png"))
