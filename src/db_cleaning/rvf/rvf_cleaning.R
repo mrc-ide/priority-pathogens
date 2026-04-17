4b8809d28476bd843dd644b961d1eb17bbb5185d
Alba Halliday <ahallid1@ic.ac.uk>
2026-04-17 16:02:47 +0100

    add generic rvf_cleaning_config.R and rvf specific cleaning rvf_cleaning.R scripts

---
+++ src/db_cleaning/rvf/rvf_cleaning.R
@@ -0,0 +1,123 @@
+# *=========================== Manual RVF cleaning ===========================*
+library(tidyr)
+
+article_cleaning <- function(df){
+  
+  # Update Not Applicable so that epireview assign_qa_score works
+  qa_cols <- c("qa_m1", "qa_m2", "qa_a3", "qa_a4", "qa_d5", "qa_d6", "qa_d7")
+  df <- df |>
+    mutate(across(all_of(qa_cols), ~ ifelse(.=="Not Applicable", NA, .)))
+  
+  # Leave first_author_first_name as is incase there are initials as the first
+  # name
+  df <- df |>
+    mutate(journal = str_to_title(journal),
+           first_author_surname = str_to_title(first_author_surname)) |>
+    mutate(journal = sub("^The\\s+", "", journal, useBytes = TRUE))
+  
+  return (df)
+}
+
+model_cleaning <- function(df){
+  return (df)
+}
+
+outbreak_cleaning <- function(df){
+  return (df)
+}
+
+param_cleaning <- function(df){
+  
+  na_replacement <- list(parameter_from_figure = "No",
+                         inverse_param = "No",
+                         exponent=0,
+                         parameter_value_type="Unspecified")
+  
+  df <- replace_na(df, na_replacement)
+  
+  no_true_false <- c("parameter_from_figure",
+                     "inverse_param")
+  
+  df[no_true_false] <- lapply(no_true_false,
+                              function(col) df[[col]] != "No")
+  
+  # Removed parameter_hd_to, parameter_hd_from since these were introduced in
+  # Redcap and somewhat duplicates capturing non standard human delays.
+  # The field other_delay_* is included in epireview, so combine columns and
+  # keep other_delay_*
+  df <- df |>
+    mutate(other_delay_start = ifelse(
+      parameter_hd_from=="Other" | is.na(parameter_hd_from),
+      other_delay_start, parameter_hd_from),
+      other_delay_end = ifelse(
+        parameter_hd_to=="Other" | is.na(parameter_hd_to),
+        other_delay_end, parameter_hd_to),
+      parameter_value_type = ifelse(parameter_value_type=="Central - unspecified",
+                                    "Unspecified", parameter_value_type)
+    ) |>
+    select(-c(parameter_hd_to, parameter_hd_from))
+  
+  df <- df |>
+    mutate(parameter_upper_bound = ifelse(
+      parameter_upper_bound=="infinity", NA, parameter_upper_bound),
+      parameter_upper_bound=as.numeric(parameter_upper_bound))
+  
+  #########################################################
+  # Some rows have text in the population_sample_size column.
+  # Move the text to the notes column and convert the column to a numeric as intended.
+  # Needs to be numeric for downstream tasks.
+  
+  # Covidence 1168 to be removed. No sampling time risk factor.
+  df <- df[-which(df$population_sample_size=="various"),]
+  
+  df <- df %>%
+    mutate(
+      is_numeric = is.na(population_sample_size) |
+        grepl("^\\s*\\d+(\\.\\d+)?\\s*$", population_sample_size),
+      parameter_notes = if_else(
+        !is_numeric,
+        paste0(
+          coalesce(parameter_notes, ""),
+          if_else(is.na(parameter_notes) | parameter_notes == "", "", " "),
+          "population_sample_size: ",
+          population_sample_size
+        ),
+        parameter_notes
+      ),
+      population_sample_size = if_else(
+        is_numeric,
+        as.numeric(population_sample_size),
+        NA_real_
+      )
+    ) %>%
+    select(-is_numeric)
+  
+  #######################################################################
+  ### Some dates entered as "XX" or "00" instead of "x" when unknown. 
+  
+  df <- df %>%
+    mutate(population_study_start_day=
+             case_when(toupper(population_study_start_day)=="XX" ~ NA,
+                       population_study_start_day=="00" ~ NA,
+                       TRUE ~ population_study_start_day),
+           population_study_end_day=
+             case_when(toupper(population_study_end_day)=="XX" ~ NA,
+                       population_study_end_day=="00" ~ NA,
+                       TRUE ~ population_study_end_day),
+           population_study_start_month=
+             case_when(toupper(population_study_start_month)=="XX" ~ NA,
+                       TRUE ~ population_study_start_month),
+           population_study_end_month=
+             case_when(toupper(population_study_end_month)=="XX" ~ NA,
+                       TRUE ~ population_study_end_month),
+           population_study_start_year=
+             case_when(toupper(population_study_start_year)=="XXXX" ~ NA,
+                       TRUE ~ population_study_start_year),
+           population_study_end_year=
+             case_when(toupper(population_study_end_year)=="XXXX" ~ NA,
+                       TRUE ~ population_study_end_year)
+    )
+  
+  return (df)
+}
+# *============================================================================*
