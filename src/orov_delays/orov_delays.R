7a18feed01300c9d2a609cf1fe16918874e4f5c3
ruthmccabe <ruth.mccabe17@imperial.ac.uk>
2026-05-01 15:59:11 +0100

    update delays task with new functions

--- src/orov_delays/orov_delays.R
+++ src/orov_delays/orov_delays.R
@@ -23,6 +23,7 @@ orderly_dependency(
 # forest plot code
 #orderly_shared_resource("lassa_functions.R" = "lassa_functions.R")
 #source("lassa_functions.R")
+orderly_shared_resource("orov_functions.R" = "orov_functions.R")
 source("orov_functions.R")
 
 # read in data
@@ -30,6 +31,277 @@ articles <- read.csv("inputs/articles.csv")
 outbreaks <- read.csv("inputs/outbreaks.csv")
 parameters <- read.csv("inputs/parameters.csv")
 
+dfs <- curation(articles, outbreaks, tibble(), parameters, plotting = TRUE)
+
+parameters <- dfs$parameters
+parameters <- parameters |> mutate(qa_score = article_qa_score / 100)
+parameters$parameter_class <- parameters$parameter_type_broad
+
+parameters <- parameters |>
+  mutate(
+    population_group = factor(
+      population_group,
+      levels = c(
+        sort(setdiff(unique(population_group), c("Other", "Unspecified"))),
+        "Other",
+        "Unspecified"
+      )
+    )
+  )
+
+d1 <- parameters |> filter(parameter_class == "Delays") %>% 
+  mutate(location_label = case_when(
+         population_country %in% c(
+           "Brazil","Trinidad and Tobago","Cuba") ~ "Local transmission",
+         population_country %in% c(
+           "Italy","Germany","Switzerland") ~ "Importation",
+         population_country=="France" & population_location== "Saul, French Guiana"  ~ "Local transmission",
+         population_country=="France" & is.na(population_location) ~ "Importation",
+         .default = population_country
+       ),
+       location_label = factor(location_label,
+                                  levels=c("Local transmission","Importation")),
+       delay_human_mosquito = case_when(
+         grepl("Human delay",parameter_type) ~ "Human only",
+         grepl("Mosquito delay",parameter_type) ~ "Mosquito only",
+         .default = "Human and Mosquito"
+       ),
+         delay_label = case_when(
+           parameter_type=="Human delay - other human delay (go to section)" ~ paste0("Human delay - ",parameter_hd_from,">",parameter_hd_to),
+           parameter_type!="Human delay - other human delay (go to section)" ~ parameter_type
+         ),
+       delay_label_short = case_when(
+         delay_label =="Human delay - symptom onset>discharge/recovery" ~ "Symptom onset -> Discharge from care",
+         delay_label=="Human delay - Symptom Onset/Fever>Seeking Care" ~ "Symptom onset -> Seeking care",
+         delay_label=="Human delay - time in care (length of stay)" ~ "Time in care",
+         delay_label=="Human delay - incubation period" ~ "Incubation period",
+         delay_label=="Human delay - symptom onset>admission to care" ~ "Symptom onset -> Admission to care",
+         delay_label=="Human delay - Symptom Onset/Fever>Discharge from Critical Care/ICU" ~ "Symptom onset -> Discharge from critical care",
+         delay_label=="Human delay - Admission to Care/Hospitalisation>Discharge from Critical Care/ICU" ~ "Admission to care -> Discharge from critical care",
+         delay_label =="Human delay - Symptom Onset/Fever>Symptom Resolution" ~ "Duration of symptoms",
+         delay_label=="Delay - human to mosquito generation time" ~ "Generation time (human to vector)",
+         delay_label=="Mosquito delay - extrinsic incubation period" ~ "Extrinsic incubation period",
+         delay_label=="Delay - mosquito to human generation time" ~ "Generation time (vector to human)",
+         delay_label=="Human delay - generation time" ~ "Generation time",
+         delay_label=="Human delay - admission to care>death" ~ "Admission to care -> Death",
+         delay_label=="Human delay - Symptom Onset/Fever>Symptom Recurrence" ~ "Symptom onset -> Symptom recurrence",
+         delay_label=="Human delay - Symptom Onset/Fever>Symptom Worsening" ~ "Symptom onset -> Symptom worsening",
+         delay_label=="Human delay - incubation period" ~ "Incubation period",
+         delay_label=="Human delay - Symptom Resolution>Symptom Recurrence" ~ "Symptom resolution -> Symptom recurrence",
+         delay_label=="Human delay - Symptom Recurrence>Seeking Care" ~ "Symptom recurrence -> Seeking care",
+         delay_label=="Human delay - Symptom Recurrence>Symptom Resolution" ~ "Symptom recurrence -> Symptom resolution",
+         delay_label=="Human delay - admission to care>discharge/recovery" ~ "Admission to care -> Recovery",
+         delay_label=="Human delay - Symptom Onset/Fever>Viral clearance" ~ "Symptom onset -> Viral clearance",
+         delay_label=="Human delay - Symptom Onset/Fever>Admission to Care/Hospitalisation" ~ "Symptom onset -> Admission to care",
+         .default = "You missed one"
+       ),
+       delay_label_short = factor(
+         delay_label_short,
+         levels = c("Incubation period",
+                    "Generation time",
+                    "Symptom onset -> Symptom worsening",
+                    "Symptom onset -> Seeking care",
+                    "Symptom onset -> Admission to care",
+                    "Admission to care -> Discharge from critical care",
+                    "Admission to care -> Recovery",
+                    "Symptom onset -> Discharge from critical care",
+                    "Admission to care -> Death",
+                    "Duration of symptoms",
+                    "Time in care",
+                    "Symptom onset -> Discharge from care",
+                    "Symptom onset -> Viral clearance",
+                    #"Symptom onset -> Death",
+                    "Symptom onset -> Symptom recurrence",
+                    "Symptom resolution -> Symptom recurrence",
+                    "Symptom recurrence -> Seeking care",
+                    "Symptom recurrence -> Symptom resolution",
+                    "Extrinsic incubation period",
+                    "Generation time (human to vector)",
+                    "Generation time (vector to human)")
+       ),
+       delay_facet_label = case_when(
+         delay_label_short %in% c("Generation time (human to vector)",
+                                  "Generation time (vector to human)",
+                                  "Generation time",
+                                  "Extrinsic incubation period") ~ "Transmission",        delay_label_short %in% c("Incubation period",
+                                  "Symptom onset -> Symptom worsening",
+                                  "Symptom onset -> Viral clearance",
+                                  "Duration of symptoms" ) ~ "Symptoms",
+         delay_label_short %in% c("Symptom onset -> Seeking care",
+                                  "Symptom onset -> Admission to care",
+                                  "Symptom onset -> Discharge from critical care",
+                                  "Symptom onset -> Discharge from care") ~ "Symptoms to care",
+       delay_label_short %in% c("Symptom onset -> Symptom recurrence",
+                                "Symptom resolution -> Symptom recurrence",
+                                "Symptom recurrence -> Seeking care",
+                                "Symptom recurrence -> Symptom resolution") ~ "Recurrence",
+       delay_label_short %in% c("Admission to care -> Discharge from critical care",
+                                "Admission to care -> Recovery",
+                                "Admission to care -> Death",
+                                "Time in care") ~ "Care to outcomes",
+       .default = "you missed one"
+))
+
+## maybe this is cheating but do a little tweak to parameter_value_type here so only this way for delays and not elsewhere
+d1 <- d1 %>% mutate(
+  parameter_value_type = case_when(
+    parameter_value_type %in% c("Unspecified","Other","Central - unspecified") ~ "Unspecified",
+    .default = parameter_value_type
+  )
+)
+
+
+#forest_plot(d1)
+qa_threshold <- -1
+qa_alpha <- 1
+point_size <- 4.5
+text_size <- 20
+
+custom_colour_pop_groups <- get_colour_pop_groups(
+  parameters,
+  d1
+)
+
+# testing it works overall
+forest_plot(
+  d1 |>
+    filter(qa_score > qa_threshold),
+  "Delays (days)",
+  "location_label",
+  c(0,40),
+  custom_colours = NA,
+  segment_show.legend = c(color = TRUE, shape = FALSE),
+  text_size = text_size,
+  qa_alpha = qa_alpha,
+  sort = TRUE,
+  point_size = point_size) + 
+  facet_wrap(~delay_label_short,scales="free_y",nrow=10)
+
+p1 <- forest_plot(
+  d1 |>
+    filter(delay_facet_label == "Symptoms"),
+  "Days",
+  "location_label",
+  c(-1,31),
+  custom_colours = NA,
+  segment_show.legend = c(color = TRUE, shape = FALSE),
+  text_size = text_size,
+  qa_alpha = qa_alpha,
+  sort = TRUE,
+  point_size = point_size) + 
+  facet_wrap(~delay_label_short,scales="free_y",ncol=1) + ggtitle("Symptoms")#+ 
+#theme(legend.position = "bottom") +  guides(colour = guide_legend(ncol = 3) )
+
+p2 <- forest_plot(
+  d1 |>
+    filter(delay_facet_label == "Symptoms to care"),
+  "Days",
+  "location_label",
+  c(-1,31),
+  custom_colours = NA,
+  segment_show.legend = c(color = TRUE, shape = FALSE),
+  text_size = text_size,
+  qa_alpha = qa_alpha,
+  sort = TRUE,
+  point_size = point_size) + 
+  facet_wrap(~delay_label_short,scales="free_y",ncol=1) + ggtitle("Symptoms to care")
+
+
+p3 <- forest_plot(
+  d1 |>
+    filter(delay_facet_label == "Care to outcomes"),
+  "Days",
+  "location_label",
+  c(-1,31),
+  custom_colours = NA,
+  segment_show.legend = c(color = TRUE, shape = FALSE),
+  text_size = text_size,
+  qa_alpha = qa_alpha,
+  sort = TRUE,
+  point_size = point_size) + 
+  facet_wrap(~delay_label_short,scales="free_y",ncol=1)+ ggtitle("Care to outcome")
+
+
+p4 <- forest_plot(
+  d1 |>
+    filter(delay_facet_label == "Recurrence"),
+  "Days",
+  "location_label",
+  c(-1,31),
+  custom_colours = NA,
+  segment_show.legend = c(color = TRUE, shape = FALSE),
+  text_size = text_size,
+  qa_alpha = qa_alpha,
+  sort = TRUE,
+  point_size = point_size) + 
+  facet_wrap(~delay_label_short,scales="free_y",ncol=1) + ggtitle("Recurrence")
+
+
+p5 <- forest_plot(
+  d1 |>
+    filter(qa_score > qa_threshold,
+           delay_facet_label == "Transmission"),
+  "Days",
+  "location_label",
+  c(-1,31),
+  custom_colours = NA,
+  segment_show.legend = c(color = TRUE, shape = FALSE),
+  text_size = text_size,
+  qa_alpha = qa_alpha,
+  sort = TRUE,
+  point_size = point_size) + 
+  facet_wrap(~delay_label_short,scales="free_y",ncol=1) + ggtitle("Transmission")
+
+cowplot::plot_grid(p1+theme(legend.position = "none"),
+                   p2+theme(legend.position = "none"),
+                   p3+theme(legend.position = "none"),
+                   p4+theme(legend.position = "none"),
+                   p5+theme(legend.position = "none"),ncol=5,
+                   rel_widths = c(1,1,1,1,0.8))
+ggsave("test.png",width=30,height=20)
+
+
+#### this isn't bad
+cowplot::plot_grid(p1+theme(legend.position = "none"),
+                   p2+theme(legend.position = "none"),
+                   p4+theme(legend.position = "none"),
+                   p3+theme(legend.position = "none"),
+                   p5+theme(legend.position = "none"),
+                   cowplot::get_legend(p1),
+                   ncol=3,
+                   rel_widths = c(1,1,1),rel_heights = c(1.5,1),
+                   align="hv")
+ggsave("test.png",width=25,height=25)
+
+
+
+
+cowplot::plot_grid(p1,p2)
+
+cowplot::plot_grid(p2+theme(legend.position = "none"),
+                   p3+theme(legend.position = "none"),
+                   p4+theme(legend.position = "none"),
+                   p5+theme(legend.position = "none"),
+                   ncol=1,align="hv",rel_heights = c(3,4,3,1))
+
+
+c2 <- cowplot::plot_grid(p2+theme(legend.position = "none"),
+                   p3+theme(legend.position = "none"),
+                   ncol=1,align="hv")
+
+c3 <- cowplot::plot_grid(p4+theme(legend.position = "none"),
+                   p5+theme(legend.position = "none"),
+                   ncol=1,align="hv",rel_heights = c(3,1))
+
+cowplot::plot_grid(c3,cowplot::get_legend(p1),ncol=1)
+
+
+
+cowplot::plot_grid(p1+theme(legend.position="none"),
+                   c2,
+                   cowplot::plot_grid(c3,cowplot::get_legend(p1),ncol=1),
+                   ncol=3,rel_widths = c(1.5,1.5,1))
+ggsave("test.png",height=20,width=25,bg="white")
 
 # ## for the purposes of plotting sort the variability/uncertainty thing 
 # Barbiero 2025
@@ -49,39 +321,39 @@ parameters <- read.csv("inputs/parameters.csv")
 # delays <- parameters %>% filter(parameter_type_broad=="Delays")
 # write.csv(delays,"delays_reextraction.csv",row.names = FALSE)
 
-# ## human delays
-## this needs to be updated to account for variability/uncertainty 
-delays <- parameters %>% filter(parameter_type_broad=="Delays") %>%
-  select(covidence_id,parameter_type,parameter_value,
-         parameter_lower_bound,parameter_upper_bound,
-         parameter_unit,parameter_value_type,parameter_statistical,
-         parameter_uncertainty_single_value,
-         parameter_uncertainty_singe_type,
-         parameter_hd_from,parameter_hd_to,
-         method_disaggregated,method_disaggregated_by,
-         parameter_context_human,parameter_context_genotype,population_sex,
-         population_sample_size,	parameter_context_sample_type,
-         population_sample_type,	population_group,	population_age_min,
-         population_age_max,	population_country,	population_location,
-         parameter_context_timing,	parameter_context_zika,
-         parameter_context_urban,	parameter_context_data,	exponent,
-         population_study_start_day,	population_study_start_month,
-         population_study_start_year,	population_study_end_day,
-         population_study_end_month,	population_study_end_year
-  )
-write.csv(delays,
-          "delay_clean.csv",row.names=FALSE)
+# # ## human delays
+# ## this needs to be updated to account for variability/uncertainty 
+# delays <- parameters %>% filter(parameter_type_broad=="Delays") %>%
+#   select(covidence_id,parameter_type,parameter_value,
+#          parameter_lower_bound,parameter_upper_bound,
+#          parameter_unit,parameter_value_type,parameter_statistical,
+#          parameter_uncertainty_single_value,
+#          parameter_uncertainty_singe_type,
+#          parameter_hd_from,parameter_hd_to,
+#          method_disaggregated,method_disaggregated_by,
+#          parameter_context_human,parameter_context_genotype,population_sex,
+#          population_sample_size,	parameter_context_sample_type,
+#          population_sample_type,	population_group,	population_age_min,
+#          population_age_max,	population_country,	population_location,
+#          parameter_context_timing,	parameter_context_zika,
+#          parameter_context_urban,	parameter_context_data,	exponent,
+#          population_study_start_day,	population_study_start_month,
+#          population_study_start_year,	population_study_end_day,
+#          population_study_end_month,	population_study_end_year
+#   )
+# write.csv(delays,
+#           "delay_clean.csv",row.names=FALSE)
 
 
-delays <- parameters %>% filter(parameter_type_broad=="Delays") %>% 
-  mutate(
-  delay_label = case_when(
-    parameter_type=="Human delay - other human delay (go to section)" ~ paste0("Human delay - ",parameter_hd_from,">",parameter_hd_to),
-    parameter_type!="Human delay - other human delay (go to section)" ~ parameter_type
-  )
-)
-
-delays %>% filter(population_sample_size==1) %>% nrow()
+# delays <- parameters %>% filter(parameter_type_broad=="Delays") %>% 
+#   mutate(
+#   delay_label = case_when(
+#     parameter_type=="Human delay - other human delay (go to section)" ~ paste0("Human delay - ",parameter_hd_from,">",parameter_hd_to),
+#     parameter_type!="Human delay - other human delay (go to section)" ~ parameter_type
+#   )
+# )
+# 
+# delays %>% filter(population_sample_size==1) %>% nrow()
 
 # delays %>% select(parameter_type,parameter_hd_from,parameter_hd_to,delay_label) %>% View()
 # 
@@ -89,10 +361,10 @@ delays %>% filter(population_sample_size==1) %>% nrow()
 
 delays_cleaned <- data_curation(articles,delays, TRUE, FALSE)
 
-forest_plot(df = delays_cleaned$parameters,
-            label = "delays",
-            color_column = "parameter_type",
-            lims = c(0,60))
+# forest_plot(df = delays_cleaned$parameters,
+#             label = "delays",
+#             color_column = "parameter_type",
+#             lims = c(0,60))
 
 delays_cleaned$parameters$central
 
