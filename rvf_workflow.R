65307b5de7bc4f4faa9903b766eaf17e784f18fb
Alba Halliday <ahallid1@ic.ac.uk>
2026-05-22 14:28:31 +0100

    update set up to be RVF specific

--- rvf_workflow.R
+++ rvf_workflow.R
@@ -1,6 +1,7 @@
 library(orderly)
-
-#remotes::install_github("mrc-ide/orderly.sharedfile")
+# 
+# remotes::install_github("mrc-ide/orderly.sharedfile")
+# orderly::orderly_init()
 
 # *--------------------------- Download REDCap data ---------------------------*
 # Downloads the data report from REDCap
@@ -19,9 +20,9 @@ orderly_run("db_redcap_download",list(pathogen="RVF"))
 # Requires:
 #   - target_table.csv (relative path: shared/redcap_data/target_table.csv)
 #   - mapping_table.csv
-#     (relative path: shared/redcap_data/nipah/mapping_table.csv)
+#     (relative path: shared/redcap_data/rvf/mapping_table.csv)
 #   - config.yaml that specifies how to run the task
-#     (relative path: src/db_extraction_prep/redcap_task/nipah/config.yaml)
+#     (relative path: src/db_extraction_prep/redcap_task/rvf/config.yaml)
 orderly_run("db_extraction_prep",list(pathogen="RVF",
                                       orderly_download_dependency=TRUE))
 
