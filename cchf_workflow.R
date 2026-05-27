e806e5061975eb45eb29a126df040b53f2d3e655
tristan-myles <mylesnaidoo95@gmail.com>
2026-05-27 17:35:01 +0100

    Adds CCHF workflow up to db_double

---
+++ cchf_workflow.R
@@ -0,0 +1,32 @@
+library(orderly)
+
+# *--------------------------- Download REDCap data ---------------------------*
+# Downloads the data report from REDCap
+# Requires:
+#   - A REDCap api
+#   - cchf_config.yaml.that specifies how to run the task
+#     (relative path: src/db_redcap_download/download_config/cchf_config.yaml)
+orderly_run("db_redcap_download",list(pathogen="CCHF"))
+
+# *----------------- Prepare data to generate extraction csvs -----------------*
+# Prepares the REDCap data so that double and single extraction csvs can be
+# created in db_extraction
+# Requires:
+#   - target_table.csv (relative path: shared/redcap_data/target_table.csv)
+#   - mapping_table.csv
+#     (relative path: shared/redcap_data/cchf/mapping_table.csv)
+#   - config.yaml that specifies how to run the task
+#     (relative path: src/db_extraction_prep/redcap_task/cchf/config.yaml)
+orderly_run("db_extraction_prep",list(pathogen="CCHF",
+                                      orderly_download_dependency=TRUE))
+
+# *------------------------- Generate extraction csvs -------------------------*
+# Extracts double and single extraction csvs used the .rds file from
+# db_extraction_prep
+orderly_run("db_extraction",list(pathogen="CCHF"))
+
+# *-------------------------- Generate fixing files ---------------------------*
+# Check if any double extractions need to be fixed and generate necessary files
+orderly_run("db_double",list(pathogen="CCHF"))
+
+
